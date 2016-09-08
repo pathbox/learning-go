dd7101230088983e0c7c826136aae73c6141d45a
luodaokai <1064569731@qq.com>
2016-09-08 22:19:48 +0800

    etcd

---
+++ src/etcd/simple.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/coreos/etcd/client"
+	"golang.org/x/net/context"
+)
+
+func main() {
+  cfg := client.Config{
+    Endpoints: []string{"http://127.0.0.1:2379"},
+    Transport: client.DefaultTransport,
+    HeaderTimeoutPerRequest: time.Second,
+  }
+  c, err := client.New(cfg)
+  if err != nil {
+    log.Fatal(err)
+  }
+  kapi := client.NewKeysAPI(c)
+  log.Print("Setting '/foo' key with 'bar' value")
+  resp, err := kapi.Set(context.Background(), "/foo", "bar", nil)
+  if err != nil {
+    log.Fatal(err)
+  } else {
+    log.Printf("Set is done. Metadata is %q\n", resp)
+  }
+  log.Print("Getting '/foo' key value")
+  resp, err = kapi.Get(context.Background(), "/foo", nil)
+  if err != nil {
+    log.Fatal(err)
+  }else {
+    log.Printf("Get is done. Metadata is %q\n", resp)
+
+    log.Printf("%q key has %q value\n", resp.Node.Key, resp.Node.Value)
+  }
+}
