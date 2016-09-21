9908ca3d85adb7c504967a91636b71ebe3a05c00
luodaokai <1064569731@qq.com>
2016-09-21 22:16:49 +0800

    header(web-net-http)

---
+++ src/web_net_http/header.go
@@ -0,0 +1,14 @@
+func handler(w http.ResponseWriter, r *http.Request){
+  fmt.Fprintf(w, "%s %s\n", r.Method, r.URL, r.Proto)
+  for k, v := range r.Header {
+    f,t.Fprintf(w, "Header[%q] = %q\n", k, v)
+  }
+  fmt.Fprintf(w, "Host = %q\n", r.Host)
+  fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+  if err := r.ParseForm(); err != nil {
+    log.Print(err)
+  }
+  for k, v := range r.Form {
+    fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+  }
+}
