2c0fe70e1e6ecbd67010808f878652b897d8822b
luodaokai <1064569731@qq.com>
2016-09-22 22:39:49 +0800

    go-json-rest look up ip

---
+++ src/go-json-rest/lookup.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "github.com/ant0ine/go-json-rest/rest"
+    "log"
+    "net"
+    "net/http"
+)
+
+func main() {
+    api := rest.NewApi()
+    api.Use(rest.DefaultDevStack...)
+    router, err := rest.MakeRouter(
+        rest.Get("/lookup/#host", func(w rest.ResponseWriter, req *rest.Request) {
+            ip, err := net.LookupIP(req.PathParam("host"))
+            if err != nil {
+                rest.Error(w, err.Error(), http.StatusInternalServerError)
+                return
+            }
+            w.WriteJson(&ip)
+        }),
+    )
+    if err != nil {
+        log.Fatal(err)
+    }
+    api.SetApp(router)
+    log.Fatal(http.ListenAndServe(":8080", api.MakeHandler()))
+}
