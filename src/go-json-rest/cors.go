d1782219841bbc54b9f57e5e856eca260fb5b62f
luodaokai <1064569731@qq.com>
2016-09-25 22:05:33 +0800

    cors go-json-rest

---
+++ src/go-json-rest/cors.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+    "github.com/ant0ine/go-json-rest/rest"
+    "log"
+    "net/http"
+)
+
+func main() {
+  api := rest.NewApi()
+  api.Use(rest.DefaultDevStack...)
+  api.Use(&rest.CorsMiddleware{
+    RejectNonCorsRequests: false,
+    OriginValidator: func(origin string, request *rest.Request) bool{
+      return origin == "http://my.other.host"
+    },
+    AllowedMethods: []string{"GET", "POST", "PUT"},
+    AllowedHeaders: []string{
+      "Accept", "Content-Type", "X-Custom-Header", "Origin"
+    },
+    AccessControlAllowCredentials: true,
+    AccessControlMaxAge:           3600,
+  })
+  router, err := rest.MakeRouter(
+    rest.Get("/conutries", GetAllCountries),
+  )
+  if err != nil{
+    log.Fatal(err)
+  }
+  api.SetApp(router)
+  log.Fatal(http.ListenAndServe(":8080", api.MakeHandler()))
+}
+
+type Country struct{
+  Code string
+  Name string
+}
+
+func GetAllCountries(w rest.ResponseWriter, r *rest.Request)  {
+  w.WriteJson(
+    []Country{
+      Country{
+        Code: "FR",
+        Name: "France",
+      },
+      Country{
+        Code: "US",
+        Name: "United States",
+      }
+    }
+  )
+
+}
