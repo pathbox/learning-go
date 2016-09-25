38118319c3d7441ffce1c5f47f414627d01adcf6
luodaokai <1064569731@qq.com>
2016-09-25 22:23:17 +0800

    basic auth

---
+++ src/go-json-rest/basic-auth.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "github.com/ant0ine/go-json-rest/rest"
+    "log"
+    "net/http"
+)
+
+func main() {
+  api := rest.NewApi()
+  api.Use(rest.DefaultDevStack...)
+  api.Use(&rest.AuthBasicMiddleware{
+    Realm: "test zone",
+    Authenticator: func(userId string, password string) bool{
+      if userId == "admin" && password == "password" {
+        return true
+      }
+      return false
+    },
+  })
+  api.SetApp(rest.AppSimple(func(w rest.ResponseWriter, r *rest.Request){
+    w.WriteJson(map[string]string{"Body": "Hello World!"})
+    }))
+
+    log.Fatal(http.ListenAndServe(":8081", api.MakeHandler()))
+}
