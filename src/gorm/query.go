4ffd35d186b4f7b2cbc58247122520d52d675520
luodaokai <1064569731@qq.com>
2016-09-29 22:56:56 +0800

    query

---
+++ src/gorm/query.go
@@ -0,0 +1,43 @@
+Query
+
+// Get first record, order by primary key
+db.First(&user)
+//// SELECT * FROM users ORDER BY id LIMIT 1;
+
+// Get last record, order by primary key
+db.Last(&user)
+//// SELECT * FROM users ORDER BY id DESC LIMIT 1;
+
+// Get all records
+db.Find(&users)
+//// SELECT * FROM users;
+
+// Get record with primary key
+db.First(&user, 10)
+//// SELECT * FROM users WHERE id = 10;
+
+Query With Where (Plain SQL)
+
+// Get first matched record
+db.Where("name = ?", "jinzhu").First(&user)
+//// SELECT * FROM users WHERE name = 'jinzhu' limit 1;
+
+// Get all matched records
+db.Where("name = ?", "jinzhu").Find(&users)
+//// SELECT * FROM users WHERE name = 'jinzhu';
+
+db.Where("name <> ?", "jinzhu").Find(&users)
+
+// IN
+db.Where("name in (?)", []string{"jinzhu", "jinzhu 2"}).Find(&users)
+
+// LIKE
+db.Where("name LIKE ?", "%jin%").Find(&users)
+
+// AND
+db.Where("name = ? AND age >= ?", "jinzhu", "22").Find(&users)
+
+// Time
+db.Where("updated_at > ?", lastWeek).Find(&users)
+
+db.Where("created_at BETWEEN ? AND ?", lastWeek, today).Find(&users)
