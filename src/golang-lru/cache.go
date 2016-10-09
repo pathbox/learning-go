fd2610da7e85298bfc00ac72fbe7924ec7d375f8
luodaokai <1064569731@qq.com>
2016-10-09 21:10:55 +0800

    golang-lru

---
+++ src/golang-lru/cache.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/golang-lru"
+)
+
+func main() {
+  l, err := lru.New(20)
+  if err != nil{
+    return
+  }
+
+  for i := 0; i < 20; i++ {
+    l.Add(i, i)
+  }
+  // if l.Len() != 10{
+  //   panic(fmt.Sprintf("bad len: %v", l.Len()))
+  // }
+
+  for i := 0; i < 20; i++{
+    fmt.Println(l.Get(i))
+  }
+}
