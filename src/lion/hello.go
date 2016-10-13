b72ae9b5ffa22ec3ffd350d1d6cc0118945a11fe
luodaokai <1064569731@qq.com>
2016-10-13 22:24:32 +0800

    lion

---
+++ src/lion/hello.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/celrenheit/lion"
+	"golang.org/x/net/context"
+)
+
+func Home(c context.Content, w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Home")
+}
+
+func Hello(c context.Context, w http.ResponseWriter, r *http.Request){
+  name := lion.Param(c, "name")
+  fmt.Fprintf(w, "Hello " + name)
+}
+
+func main() {
+  l := lion.Classic()
+  l.GetFunc("/", Home)
+  l.GetFunc("/hello/:name", Hello)
+  l.Run()
+}
