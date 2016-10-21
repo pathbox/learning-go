5fda677cad569a4a7de466c395aca7e073f42671
pathbox <1064569731@qq.com>
2016-10-21 22:01:37 +0800

    many mux handle

--- src/http/handler/main.go
+++ src/http/handler/main.go
@@ -6,12 +6,14 @@ import (
 )
 
 func main() {
-  mux := http.NewServeMux()
+	mux := http.NewServeMux()
 
-  rh := http.RedirectHandler("http://www.hao123.com", http.StatusTemporaryRedirect)
-  mux.Handle("/foo", rh)
+	rh := http.RedirectHandler("http://www.hao123.com", http.StatusTemporaryRedirect)
+	rh_baidu := http.RedirectHandler("http://www.baidu.com", http.StatusTemporaryRedirect)
+	mux.Handle("/foo", rh)
+	mux.Handle("/baidu", rh_baidu)
 
-  log.Println("Listening at :9090")
-  // http.ListenAndServe(":9090", mux)
-  log.Fatal(http.ListenAndServe(":9090", mux))
+	log.Println("Listening at :9090")
+	// http.ListenAndServe(":9090", mux)
+	log.Fatal(http.ListenAndServe(":9090", mux))
 }
