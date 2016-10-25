6dc615a0b17ee13812731b8f7e15b6807c82fc47
pathbox <1064569731@qq.com>
2016-10-25 23:47:52 +0800

    echo->quick

---
+++ src/echo/quick.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/engine/standard"
+	"net/http"
+)
+
+func main() {
+	e := echo.New()
+	e.Get("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello World!")
+	})
+	e.Run(standard.New(":9090"))
+}
