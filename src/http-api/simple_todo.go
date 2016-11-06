29116b2aa64b8105fece280a9aebbb3ee688b08d
pathbox <1064569731@qq.com>
2016-11-06 19:56:43 +0800

    http-api simple todos

---
+++ src/http-api/simple_todo.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+type Todo struct {
+	Name      string    `json:"name"`
+	Completed bool      `json:"complated"`
+	Due       time.Time `json:"due"`
+}
+
+type Todos []Todo
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/todos", TodoIndex)
+	router.HandleFunc("/todos/{todoId}", TodoShow)
+
+	log.Fatal(http.ListenAndServe(":9090", router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(" Hello, Welcome!"))
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+	todos := Todos{
+		Todo{Name: "Write presentation"},
+		Todo{Name: "Host meetup"},
+	}
+
+	json.NewEncoder(w).Encode(todos)
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoId := vars["todoId"]
+	fmt.Fprintln(w, "Todo show:", todoId)
+}
