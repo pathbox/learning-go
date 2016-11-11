ddc86090fc60b9b16ae88b349f136f92c739078a
luodaokai <luodaokai@udesk.cn>
2016-11-11 15:05:39 +0800

    缓冲 channel 的阻塞死锁与不阻塞示例

---
+++ src/misc/channel/async_chan.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	data := make(chan int, 3) // 缓冲区可以存储 3 个元素
+	exit := make(chan bool)
+
+	data <- 1 // 在缓冲区未满前，不会阻塞。
+	data <- 2
+	data <- 3
+	// data <- 33  fatal error: all goroutines are asleep - deadlock!
+
+	go func() {
+		for d := range data { // 在缓冲区未空前，不会阻塞。
+			fmt.Println(d)
+		}
+		exit <- true
+	}()
+
+	data <- 4 // 如果缓冲区已满，阻塞。
+	data <- 5
+	close(data)
+
+	<-exit
+}
