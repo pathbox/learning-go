1fb8c1239ca03bdc5d39a48fb7f5498c40b260a3
pathbox <1064569731@qq.com>
2016-11-19 21:51:19 +0800

    net.ParseIP

---
+++ src/network/ip/parseip.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s ip-addr\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	name := os.Args[1]
+	addr := net.ParseIP(name)
+	if addr == nil {
+		fmt.Println("Invalid address")
+	} else {
+		fmt.Println("The address is ", addr.String())
+	}
+	os.Exit(0)
+}
