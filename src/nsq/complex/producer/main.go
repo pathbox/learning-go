aa995f98d5a32bda5226cc600935d4c2b802b465
pathbox <1064569731@qq.com>
2016-11-23 22:55:30 +0800

    nsq complex

---
+++ src/nsq/complex/producer/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+
+	nsq "github.com/nsqio/go-nsq"
+)
+
+var (
+	logger *log.Logger
+)
+
+func main() {
+	logger = log.New(ioutil.Discard, "", log.LstdFlags)
+	config := nsq.NewConfig()
+	addr := "10.0.3.126:4150"
+	w, _ := nsq.NewProducer(addr, config)
+	defer w.Stop()
+	w.SetLogger(logger, nsq.LogLevelDebug)
+
+	msgCount := 10
+	var testData [][]byte
+	for i := 0; i < msgCount; i++ {
+		testData = append(testData, []byte("multipublish_test_case"))
+	}
+
+	topicName := "test"
+	responseChan := make(chan *nsq.ProducerTransaction)
+	err := w.MultiPublishAsync(topicName, testData, responseChan, "test", 1)
+	if err != nil {
+		logger.Fatalf(err.Error())
+	}
+
+	trans := <-responseChan
+	if trans.Error != nil {
+		logger.Fatalf(trans.Error.Error())
+	}
+	if trans.Args[0].(string) != "test" {
+		logger.Fatalf(`proxied arg "%s" != "test"`, trans.Args[0].(string))
+	}
+	if trans.Args[1].(int) != 1 {
+		logger.Fatalf(`proxied arg %d != 1`, trans.Args[1].(int))
+	}
+}
