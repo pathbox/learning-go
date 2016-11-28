459207030b18859edaa2bf82f0cc9762d656b659
pathbox <1064569731@qq.com>
2016-11-28 22:45:32 +0800

    pipeline squaring num

---
+++ src/pipeline/squaring-num.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+func gen(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func sq(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func main() {
+	c := gen(2, 3)
+	out := sq(c)
+
+	// Consume the output.
+	fmt.Println(<-out) // 4
+	fmt.Println(<-out) // 9
+
+	for n := range sq(sq(gen(2, 3))) {
+		fmt.Println(n) // 16 then 64
+	}
+}
