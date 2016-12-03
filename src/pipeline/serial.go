061b19bf9d7f63b29ee3ff5391383c4e23f85312
pathbox <1064569731@qq.com>
2016-12-03 18:44:35 +0800

    使用filepath.Walk得到一个嵌套目录下的文件，对文件内容数据进行MD5加密 存到一个map中，显示出来

---
+++ src/pipeline/serial.go
@@ -0,0 +1,53 @@
+// +build OMIT
+
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sort"
+)
+
+// MD5All reads all the files in the file tree rooted at root and returns a map
+// from file path to the MD5 sum of the file's contents.  If the directory walk
+// fails or any read operation fails, MD5All returns an error.
+
+func MD5All(root string) (map[string][md5.Size]byte, error) {
+	m := make(map[string][md5.Size]byte)
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.Mode().IsRegular() {
+			return nil
+		}
+		data, err := ioutil.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		m[path] = md5.Sum(data)
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+func main() {
+	m, err := MD5All(os.Args[1])
+	if err != nil {
+		return
+	}
+	var paths []string
+	for path := range m {
+		paths = append(paths, path)
+	}
+	sort.Strings(paths)
+	for _, path := range paths {
+		fmt.Printf("%x %s\n", m[path], path)
+	}
+}
