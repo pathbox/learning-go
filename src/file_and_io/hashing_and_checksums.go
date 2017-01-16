e2f7ae26cfdb4769fd2b2c878a866fd3803b823d
luodaokai <1064569731@qq.com>
2017-01-16 15:35:58 +0800

    :hourglass::flashlight:Archive 、 temporary and hashing files

---
+++ src/file_and_io/hashing_and_checksums.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+	data, err := ioutil.ReadFile("files/test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Hash the file and output results
+	fmt.Printf("Md5: %x\n\n", md5.Sum(data))
+	fmt.Printf("Sha1: %x\n\n", sha1.Sum(data))
+	fmt.Printf("Sha256: %x\n\n", sha256.Sum256(data))
+	fmt.Printf("Sha512: %x\n\n", sha512.Sum512(data))
+}
---
+++ src/file_and_io/hashing_sum_file.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("files/test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Create new hasher, which is a writer interface
+	hasher := md5.New()
+	_, err = io.Copy(hasher, file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Hash and print. Pass nil since
+	// the data is not coming in as a slice argument
+	// but is coming through the writer interface
+	sum := hasher.Sum(nil)
+	fmt.Printf("Md5 checksum: %x\n", sum)
+}
