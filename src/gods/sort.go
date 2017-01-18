d0d14d8c6f81976b5514ce021675615b7cc6b18e
luodaokai <1064569731@qq.com>
2017-01-18 14:25:49 +0800

    tree struct

---
+++ src/gods/sort.go
@@ -0,0 +1,13 @@
+package examples
+
+import "github.com/emirpasic/gods/utils"
+
+// SortExample to demonstrate basic usage of basic sort
+func SortExample() {
+	strings := []interface{}{}                  // []
+	strings = append(strings, "d")              // ["d"]
+	strings = append(strings, "a")              // ["d","a"]
+	strings = append(strings, "b")              // ["d","a",b"
+	strings = append(strings, "c")              // ["d","a",b","c"]
+	utils.Sort(strings, utils.StringComparator) // ["a","b","c","d"]
+}
