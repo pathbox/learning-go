79954b3f174c3c6dd80d6541a0be0326bf34fda8
luodaokai <1064569731@qq.com>
2017-02-06 19:54:15 +0800

    boltdbweb

---
+++ src/boltdweb/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/boltdb/bolt"
+	"github.com/evnix/boltdbweb/web"
+	"github.com/gin-gonic/gin"
+)
+
+const version = "v0.0.0"
+
+var (
+	showHelp   bool
+	db         *bolt.DB
+	dbName     string
+	port       string
+	staticPath string
+)
+
+func usage(appName, version string) {
+	fmt.Printf("Usage: %s [OPTIONS] [DB_NAME]", appName)
+	fmt.Printf("\nOPTIONS:\n\n")
+	flag.VisitAll(func(f *flag.Flag) {
+		if len(f.Name) > 1 {
+			fmt.Printf("    -%s, -%s\t%s\n", f.Name[0:1], f.Name, f.Usage)
+		}
+	})
+	fmt.Printf("\n\nVersion %s\n", version)
+}
+
+func init() {
+	dbName = os.Getenv("BOLTDBWEB_DB_NAME")
+	port = os.Getenv("BOLTDBWEB_PORT")
+	staticPath = os.Getenv("BOLTDBWEB_STATIC_PATH")
+
+	if staticPath == "" {
+		staticPath = "."
+	}
+	if port == "" {
+		port = "9090"
+	}
+
+	// Setup for command line processing
+	flag.BoolVar(&showHelp, "h", false, "display help")
+	flag.BoolVar(&showHelp, "help", false, "display help")
+	flag.StringVar(&dbName, "d", dbName, "Name of the database")
+	flag.StringVar(&dbName, "db-name", dbName, "Name of the database")
+	flag.StringVar(&port, "p", port, "Port for the web-ui")
+	flag.StringVar(&port, "port", port, "Port for the web-ui")
+	flag.StringVar(&staticPath, "s", staticPath, "Path for the static content")
+	flag.StringVar(&staticPath, "static-path", staticPath, "Path for the static content")
+}
+
+func main() {
+	appName := path.Base(os.Args[0])
+	flag.Parse()
+	args := flag.Args()
+
+	if showHelp == true {
+		usage(appName, version)
+		os.Exit(0)
+	}
+
+	if len(args) > 0 {
+		dbName = args[0]
+	}
+
+	if dbName == "" {
+		usage(appName, version)
+		log.Printf("\nERROR: Missing boltdb name\n")
+		os.Exit(1)
+	}
+
+	fmt.Print(" ")
+	log.Info("starting boltdb-browser..")
+
+	var err error
+	db, err = bolt.Open(dbName, 0600, nil)
+	boltbrowserweb.Db = db
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/", boltbrowserweb.Index)
+
+	r.GET("/buckets", boltbrowserweb.Buckets)
+	r.POST("/createBucket", boltbrowserweb.CreateBucket)
+	r.POST("/put", boltbrowserweb.Put)
+	r.POST("/get", boltbrowserweb.Get)
+	r.POST("/deleteKey", boltbrowserweb.DeleteKey)
+	r.POST("/deleteBucket", boltbrowserweb.DeleteBucket)
+	r.POST("/prefixScan", boltbrowserweb.PrefixScan)
+
+	r.Static("/web", staticPath+"/web")
+
+	r.Run(":" + port)
+}
