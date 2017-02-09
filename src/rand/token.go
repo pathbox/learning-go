be95da78642bb7e2b7abb039cb7c09d4d58186f8
luodaokai <1064569731@qq.com>
2017-02-09 11:29:47 +0800

    generate md5 encryp token with time

--- src/rand/token.go
+++ src/rand/token.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"crypto/rand"
-	"encoding/base64"
+	// "encoding/base64"
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
+	"time"
 )
 
 func GenerateRandomBytes(n int) ([]byte, error) {
@@ -17,7 +20,19 @@ func GenerateRandomBytes(n int) ([]byte, error) {
 
 func GenerateRandomString(s int) (string, error) {
 	b, err := GenerateRandomBytes(s)
-	return base64.URLEncoding.EncodeToString(b), err
+	// return base64.URLEncoding.EncodeToString(b), err  // base64的编码
+	return hex.EncodeToString(b), err // 16进制的编码
+}
+
+func GenerateRandomMd5StringTimeUnix(s int) (string, error) { // 为什么用md5，因为加密后能够防止别人轻易的解密
+	t := time.Now().UnixNano()
+	md5Newer := md5.New()
+	timeStr := fmt.Sprintf("%d", t)
+	salt := "718e7a3b4d2c0e32c3f78a02b62f56c5"
+	str := timeStr + salt
+	md5Newer.Write([]byte(str))
+	hexResult := hex.EncodeToString(md5Newer.Sum([]byte("aaa"))) // “aaa”len 为3, 得到的最后结果长度： 32 + 2*3 = 38
+	return hexResult, nil                                        // 得到32位长度的token字符串
 }
 
 func main() {
@@ -26,4 +41,6 @@ func main() {
 		panic(err)
 	}
 	fmt.Println("Generate Token: ", token)
+	md5Token, err := GenerateRandomMd5StringTimeUnix(32)
+	fmt.Println(md5Token)
 }
