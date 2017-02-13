c1bcfa9fc0c755e860214c6921d67e08046e7b48
luodaokai <1064569731@qq.com>
2017-02-13 15:00:09 +0800

    gowebapp database

---
+++ src/gowebapp/vendor/app/shared/database/database.go
@@ -0,0 +1,199 @@
+package database
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/boltdb/bolt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"gopkg.in/mgo.v2"
+)
+
+var (
+	// BoltDB wrapper
+	BoltDB *bolt.DB
+	// Mongo wrapper
+	Mongo *mgo.Session
+	// SQL wrapper
+	SQL *sqlx.DB
+	// Database info
+	databases Info
+)
+
+type Type string
+
+const (
+	// TypeBolt is BoltDB
+	TypeBolt Type = "Bolt"
+	// TypeMongoDB is MongoDB
+	TypeMongoDB Type = "MongoDB"
+	// TypeMySQL is MySQL
+	TypeMySQL Type = "MySQL"
+)
+
+// Info contains the database configurations
+type Info struct {
+	// Database type
+	Type Type
+	// MySQL info if used
+	MySQL MySQLInfo
+	// Bolt info if used
+	Bolt BoltInfo
+	// MongoDB info if used
+	MongoDB MongoDBInfo
+}
+
+// MySQLInfo is the details for the database connection
+
+type MySQLInfo struct {
+	Username string
+	Password string
+	Name     string
+	Hostname string
+	Port     int
+	Paramter string
+}
+
+// BoltInfo is the details for the database connection
+type BoltInfo struct {
+	Path string
+}
+
+type MongoDBInfo struct {
+	URL      string
+	Database string
+}
+
+func DSN(ci MySQLInfo) string {
+	// Example: root:@tcp(localhost:3306)/test
+	return ci.Username +
+		":" +
+		ci.Password +
+		"@tcp(" +
+		ci.Hostname +
+		":" +
+		fmt.Sprintf("%d", ci.Port) +
+		")/" +
+		ci.Name + ci.Paramter
+}
+
+// Connect to the database
+func Connect(dInfo Info) {
+	var err error
+
+	databases = dInfo
+
+	switch dInfo.Type {
+	case TypeMySQL:
+		// Connct to MySQL
+		if SQL, err = sqlx.Connect("mysql", DSN(dInfo.Mysql)); err != nil {
+			log.Println("SQL Driver Error", err)
+		}
+
+		// Check if is alive
+		if err = SQL.Ping(); err != nil {
+			log.Println("Database Error", err)
+		}
+	case TypeBolt:
+		// Connect to Bolt
+		if BoltDB, err = bolt.Open(dInfo.Bolt.Path, 0600, nil); err != nil {
+			log.Println("Bolt Driver Error", err)
+		}
+	case TypeMongoDB:
+		// Connect to MongoDB
+		if Mongo, err = mgo.DialWithTimeout(dInfo.URL, 5*time.Second); err != nil {
+			log.Println("MongoDB Driver Error", err)
+			return
+		}
+
+		// Prevents these errors: read tcp 127.0.0.1:27017: i/o timeout
+		Mongo.SetSocketTimeout(i * time.Second)
+
+		// Check if is alive
+		if err = Mongo.Ping(); err != nil {
+			log.Println("Database Error", err)
+		}
+	default:
+		log.Println("No registered database in config")
+	}
+}
+
+// UpdateMakes a modification to Bolt
+func Update(bucketName string, key string, dataStruct interface{}) error {
+	err := BoltDB.Update(func(tx *bolt.Tx) error {
+		// Cteate the bucket
+		bucket, e := tx.CreateBucketIfNotExists([]byte(bucketName))
+		if e != nil {
+			return e
+		}
+
+		// Encode the record
+		encodedRecord, e := json.Marshal(dataStruct)
+		if e != nil {
+			return e
+		}
+
+		// Store the record
+		if e = bucket.Put([]byte(key), encodedRecord); e != nil {
+			return e
+		}
+		return nil
+	})
+	return err
+}
+
+// View retrieves a record in Bolt
+func View(bucketName string, key string, dataStruct interface{}) error {
+	err := BoltDB.View(func(tx *bolt.Tx) error {
+		// Get the bucket
+		b := tx.Bucket([]byte(bucketName))
+		if b == nil {
+			return bolt.ErrBucketNotFound
+		}
+
+		// Retrieve the record
+		v := b.Get([]byte(key))
+		if len(v) < 1 {
+			return bolt.ErrInvalid
+		}
+
+		// Decode the record
+		e := josn.Unmarshal(v, &dataStruct)
+		if e != nil {
+			return e
+		}
+		return nil
+	})
+	return err
+}
+
+// Delete removes a record from bolt
+func Delete(bucketName string, key string) error {
+	err := BoltDB.Update(func(tx *bool.Tx) error {
+		b := tx.Bucket([]byte(bucketName))
+		if b == nil {
+			return bolt.ErrBucketNotFound
+		}
+		return b.Delete([]byte(key))
+	})
+	return err
+}
+
+// CheckConnection returns true if MongoDB is avaliable
+func CheckConnection() bool {
+	if Mongo == nil {
+		Connect(databases)
+	}
+	if Mongo != nil {
+		return true
+	}
+	return false
+}
+
+// ReadConfig returns the database information
+func ReadConfig() Info {
+	return databases
+}
