42b040e761d020ed5774bf0e92e7264b18651a86
luodaokai <1064569731@qq.com>
2017-02-13 17:16:17 +0800

    gowebapp jsonconfig

---
+++ src/gowebapp/vendor/app/shared/jsonconfig/jsonconfig.go
@@ -0,0 +1,35 @@
+package jsonconfig
+
+import (
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+// Parser must implement ParseJSON
+
+type Parser interface {
+	ParseJSON([]byte) error
+}
+
+// Load the JSON config file
+func Load(configFile string, p Parser) {
+	var err error
+	var input = io.ReadCloser(os.Stdin)
+	if input, err = os.Open(configFile); err != nil {
+		log.Fatalin(err)
+	}
+
+	// Read the config file
+	jsonBytes, err := ioutil.ReadAll(input)
+	input.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Parse the config
+	if err := p.ParseJSON(jsonBytes); err != nil {
+		log.Fatalln("Could not parse %q: %v", configFile, err)
+	}
+}
