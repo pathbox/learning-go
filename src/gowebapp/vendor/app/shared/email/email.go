1ef9afbff238ecb1712b06e92f86dcd22c3518a7
luodaokai <1064569731@qq.com>
2017-02-13 17:06:33 +0800

    gowebapp email

---
+++ src/gowebapp/vendor/app/shared/email/email.go
@@ -0,0 +1,59 @@
+package email
+
+import (
+	"encoding/base64"
+	"fmt"
+	"net/smtp"
+)
+
+var (
+	e SMTPInfo
+)
+
+type SMTPInfo struct {
+	Username string
+	Password string
+	Hostname string
+	Port     int
+	From     string
+}
+
+// Configure adds the settings for SMTP server
+func Configure(c SMTPInfo) {
+	e = c
+}
+
+// ReadConfig returns the SMTP information
+func ReadConfig() SMTPInfo {
+	return e
+}
+
+// SendEmail sends an email
+func SendEmail(to, subject, body string) error {
+	auth := smtp.PlainAuth("", e.Username, e.Password, e.Hostname)
+
+	header := make(map[string]string)
+	header["From"] = e.From
+	header["To"] = to
+	header["Subject"] = subject
+	header["MIME-Version"] = "1.0"
+	header["Content-Type"] = `text/plain; charset="utf-8"`
+	header["content-Transfer-Encoding"] = "base64"
+
+	message := ""
+	for k, v := range header {
+		message += fmt.Sprintf("%s: %s\r\n", k, v)
+	}
+	message += "\r\n" + base64.StdEncoding.EncodeToString([]byte(body))
+
+	// Send the email
+	err := smtp.SendMail{
+		fmt.Sprintf("%s:%d", e.Hostname, e.Port),
+		auth,
+		e.Form,
+		[]string{to},
+		[]byte(message),
+	}
+
+	return err
+}
