e5ca5f4004b4736feba3b51e68c78bfad39d9a2c
luodaokai <1064569731@qq.com>
2017-02-24 10:04:54 +0800

    again fileinput bufio

---
+++ src/file_and_io/again/fileinput.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	inputFile, inputError := os.Open("input.dat")
+	if inputError != nil {
+		fmt.Printf("An error occurred on opening the inputfile\n" +
+			"Does the file exist?\n" +
+			"Have you got acces to it?\n")
+		return // exit the function on error
+	}
+	defer inputFile.Close()
+
+	inputReader := bufio.NewReader(inputFile) // 缓存读取，并不是一次全部放入内存读取，而是分批读取，这样虽然会慢，但是节省了内存
+	for {
+		inputString, readerError := inputReader.ReadString('\n')
+		if readerError == io.EOF {
+			return
+		}
+		fmt.Printf("The input was: %s", inputString)
+	}
+}
