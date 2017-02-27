1f15f41dbcbb74f2b1de1c582b5b1f0870bebd21
luodaokai <1064569731@qq.com>
2017-02-27 20:43:11 +0800

    addition channel

--- src/channel/lock_channel2.go
+++ src/channel/lock_channel2.go
@@ -9,7 +9,7 @@ func main() {
 	c := make(chan int)
 	// val := <-c //it not right， locking
 	go func() {
-		time.Sleep(time.Second * 3)
+		time.Sleep(time.Second * 1)
 		c <- 60
 	}()
 	val := <-c
@@ -21,4 +21,8 @@ func main() {
 对同一个channel 的 read 和write 要在不同的goroutine中进行，在同一个goroutine中会死锁
 从channel中的读取操作(<-c)代码顺序要在要在写入channel的操作之后(c<-) 要不也会产生死锁
 从channel中读取操作会等待写入channel数据操作的执行，有数据了才能读嘛，channel读取操作会一直阻塞
+*/
+
+/*
+先进行写channel 的 goroutine代码，再进行读 channel的goroutine代码，读写逻辑分别在不同的goroutine；读channel会一直阻塞读的goroutine，直到读取出数据，除非用缓存channel。 读channel的goroutine知道有写channel的goroutine存在，然后就一直等数据过来，读取到之后，才结束阻塞
 */
