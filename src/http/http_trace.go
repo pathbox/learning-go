8a37b60926d824389195992810f5fc5a632825b6
luodaokai <1064569731@qq.com>
2017-03-02 10:18:55 +0800

    http trace

---
+++ src/http/http_trace.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptrace"
+)
+
+func main() {
+	req, _ := http.NewRequest("GET", "https://baidu.com", nil)
+	trace := &httptrace.ClientTrace{
+		GotConn: func(connInfo httptrace.GotConnInfo) {
+			fmt.Printf("Got Conn: %+v\n", connInfo)
+		},
+		DNSDone: func(dnsinfo httptrace.DNSDoneInfo) {
+			fmt.Printf("DNS Info; %+v\n", dnsinfo)
+		},
+	}
+	req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
+	_, err := http.DefaultTransport.RoundTrip(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
