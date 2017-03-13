1a7acdb030ec2cec8b6c85f24d2529af7335e91c
luodaokai <1064569731@qq.com>
2017-03-13 18:32:04 +0800

    userip

---
+++ src/IP/userip/userip.go
@@ -0,0 +1,35 @@
+package userip
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+
+	"context"
+)
+
+func FromRequest(req *http.request) (net.IP, error) {
+	ip, _, err := net.SplitHostPort(req.RemoteAddr)
+	if err != nil {
+		return nil, fmt.Errorf("userip: %q is not IP:port", req.RemoteAddr)
+	}
+
+	userIP := net.ParseIP(ip)
+	if userIP == nil {
+		return nil, fmt.Errorf("userip: %q is not IP:port", req.RemoteAddr)
+	}
+	return userIP, nil
+}
+
+type key int
+
+const userIPKey key = 0
+
+func NewContext(ctx context.Context, userIP net.IP) context.Context {
+	return context.WithValue(ctx, userIPKey, userIP)
+}
+
+func FromContext(ctx context.Context) (net.IP, bool) {
+	userIP, ok := ctx.Value(userIPKey).(net.IP)
+	return userIP, ok
+}
