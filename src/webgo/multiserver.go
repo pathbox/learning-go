851c2ecf6c458b09dcbfaad0fa0e1cca824f302d
luodaokai <1064569731@qq.com>
2017-03-16 17:21:23 +0800

    webgo(multiserver)

---
+++ src/webgo/multiserver.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/hoisie/web"
+)
+
+func hello1(val string) string { return "hello1 " + val + "\n" }
+
+func hello2(val string) string { return "hello2 " + val + "\n" }
+
+func main() {
+	var server1 web.Server
+	var server2 web.Server
+
+	server1.Get("/(.*)", hello1)
+	go server1.Run(":9090")
+	server2.Get("/(.*)", hello2)
+	go server2.Run("0.0.0.0:9091")
+	<-make(chan int)
+}
