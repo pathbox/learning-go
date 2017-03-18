cc58fe3cd8ff378770a1143732978e1ce9845da9
luodaokai <1064569731@qq.com>
2017-03-18 17:23:50 +0800

    webgo source web.go

---
+++ src/webgo/source/webgo/web.go
@@ -0,0 +1,186 @@
+package web
+
+import (
+	"crypto/tls"
+	"golang.org/x/net/websocket"
+	"log"
+	"mime"
+	"net/http"
+	"os"
+	"path"
+	"reflect"
+	"strings"
+)
+
+// A Context object is created for every incoming HTTP request, and is
+// passed to handlers as an optional first argument. It provides information
+// about the request, including the http.Request object, the GET and POST params,
+// and acts as a Writer for the response.
+type Context struct {
+	Request *http.Request
+	Params  map[string]string
+	Server  *Server
+	http.ResponseWriter
+}
+
+// WriteString writes string data into the response object.
+func (ctx *Context) WriteString(content string) {
+	ctx.ResponseWriter.Write([]byte(content))
+}
+
+// Abort is a helper method that sends an HTTP header and an optional
+// body. It is useful for returning 4xx or 5xx errors.
+// Once it has been called, any return value from the handler will
+// not be written to the response.
+
+func (ctx *Context) Abort(status int, body string) {
+	ctx.SetHeader("Content-Type", "text/html; charset=utf-8", true)
+	ctx.ResponseWriter.WriteHeader(status)
+	ctx.ResponseWriter.Write([]byte(body))
+}
+
+// Redirect is a helper method for 3xx redirects
+func (ctx *Context) Redirect(status int, url_ string) {
+	ctx.ResponseWriter.Header().Set("Location", url_)
+	ctx.ResponseWriter.WriteHeader(status)
+	ctx.ResponseWriter.Write([]byte("Redirecting to: " + url_))
+}
+
+// BadRequest writes  a 400 HTTP response
+func (ctx *Context) BadRequest() {
+	ctx.ResponseWriter.WriteHeader(400)
+}
+
+// Notmodified writes a 304 HTTP response
+func (ctx *Context) NotModified() {
+	ctx.ResponseWriter.WriteHeader(304)
+}
+
+func (ctx *Context) NotFound(message string) {
+	ctx.ResponseWriter.WriteHeader(404)
+	ctx.ResponseWriter.Write([]byte(message))
+}
+
+// ContentType sets the Content-Type header for an HTTP response.
+// For example, ctx.ContentType("json") sets the content-type to "application/json"
+// If the supplied value contains a slash (/) it is set as the Content-Type
+// verbatim. The return value is the content type as it was
+// set, or an empty string if none was found.
+
+func (ctx *Context) ContentType(val string) string {
+	var ctype string
+	if strings.ContainsRune(val, "/") {
+		ctype = val
+	} else {
+		if !string.HasPrefix(val, ".") {
+			val = "." + val
+		}
+		ctype = mime.TypeByExtension(val)
+	}
+	if ctype != "" {
+		ctx.Header().Set("Content-Type", ctype)
+	}
+	return ctype
+}
+
+// SetHeader sets a response header. If `unique` is true, the current value
+// of that header will be overwritten . If false, it will be appended.
+func (ctx *Context) SetHeader(hdr string, val string, unique bool) {
+	if unique {
+		ctx.Header().Set(hdr, val)
+	} else {
+		ctx.Header().Add(hdr, val)
+	}
+}
+
+func (ctx *Context) SetCookie(cookie *http.Cookie) {
+	ctx.SetHeader("Set-Cookie", cookie.String(), false)
+}
+
+var contextType reflect.Type
+
+var defaultStaticDirs []string
+
+func init() {
+	contextType = reflect.TypeOf(Context{})
+	wd, _ := os.Getwd() // pwd
+	arg0 := path.Clean(os.Args[0])
+	var exeFile string
+	if strings.HasPrefix(arg0, "/") {
+		exeFile = arg0
+	} else {
+		exeFile = path.Join(wd, arg0)
+	}
+	parent, _ := path.Split(exeFile)
+	defaultStaticDirs = append(defaultStaticDirs, path.Join(parent, "static"))
+	defaultStaticDirs = append(defaultStaticDirs, path.Join(wd, "static"))
+	return
+}
+
+func Process(c http.ResponseWriter, req *http.Request) {
+	mainServer.Process(c, req)
+}
+
+func Run(addr string) {
+	mainServer.Run(addr)
+}
+
+func RunTLS(addr string, config *tls.Config) {
+	mainServer.RunTLS(addr, config)
+}
+
+func RunFcgi(addr string) {
+	mainServer.RunFcgi(addr)
+}
+
+func Close() {
+	mainServer.Close()
+}
+
+// Get adds a handler for the 'GET' http method in the main server.
+func Get(route string, handler interface{}) {
+	mainServer.Get(route, handler)
+}
+
+// Post adds a handler for the 'POST' http method in the main server.
+func Post(route string, handler interface{}) {
+	mainServer.addRoute(route, "POST", handler)
+}
+
+// Put adds a handler for the 'PUT' http method in the main server.
+func Put(route string, handler interface{}) {
+	mainServer.addRoute(route, "PUT", handler)
+}
+
+// Delete adds a handler for the 'DELETE' http method in the main server.
+func Delete(route string, handler interface{}) {
+	mainServer.addRoute(route, "DELETE", handler)
+}
+
+// Match adds a handler for an arbitrary http method in the main server.
+func Match(method string, route string, handler interface{}) {
+	mainServer.addRoute(route, method, handler)
+}
+
+// Add a custom http.Handler. Will have no effect when running as FCGI or SCGI.
+func Handle(route string, method string, httpHandler http.Handler) {
+	mainServer.Handle(route, method, httpHandler)
+}
+
+//Adds a handler for websockets. Only for webserver mode. Will have no effect when running as FCGI or SCGI.
+func Websocket(route string, httpHandler websocket.Handler) {
+	mainServer.Websocket(route, httpHandler)
+}
+
+// SetLogger sets the logger for the main server.
+func SetLogger(logger *log.Logger) {
+	mainServer.Logger = logger
+}
+
+// Config is the configuration of the main server.
+var Config = &ServerConfig{
+	RecoverPanic: true,
+	ColorOutput:  true,
+}
+
+var mainServer = NewServer()
