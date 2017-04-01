7bb2f76e291757625f699f2a3ba8565f45738203
luodaokai <luodaokai@udesk.cn>
2017-04-01 09:57:57 +0800

    server.Accept() do what

--- src/go-engine.io/example.go
+++ src/go-engine.io/example.go
@@ -13,7 +13,7 @@ func main() {
 
 	go func() {
 		for {
-			conn, _ := server.Accept()
+			conn, _ := server.Accept() // Accept成功了，才会继续往下走，建立一个goroutine，处理这个连接，要不会一直阻塞在Accept，等待新的连接到来。所以，Accept需要写在死循环中，要不只会生成一个连接，逻辑就结束了
 			go func() {
 				defer conn.Close()
 				for {
