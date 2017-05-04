3425e9b2b4f421f91aa55e709709713524a07235
luodaokai <luodaokai@udesk.cn>
2017-05-04 17:41:13 +0800

    benchmark_concat_string.go

---
+++ src/string/benchmark_concat_string.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+func BenchmarkConcat(b *testing.B) {
+	var str string
+	for n := 0; n < b.N; n++ {
+		str += "x"
+	}
+	b.StopTimer()
+
+	if s := strings.Repeat("x", b.N); str != s {
+		b.Errorf("unexpected result; got=%s, want=%s", str, s)
+	}
+}
+
+func BenchmarkBuffer(b *testing.B) {
+	var buffer bytes.Buffer
+	for n := 0; n < b.N; n++ {
+		buffer.WriteString("x")
+	}
+	b.StopTimer()
+
+	if s := strings.Repeat("x", b.N); buffer.String() != s {
+		b.Errorf("unexpected result; got=%s, want=%s", buffer.String(), s)
+	}
+}
+
+func BenchmarkCopy(b *testing.B) {
+	bs := make([]byte, b.N)
+	bl := 0
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		bl += copy(bs[bl:], "x")
+	}
+	b.StopTimer()
+
+	if s := strings.Repeat("x", b.N); string(bs) != s {
+		b.Errorf("unexpected result; got=%s, want=%s", string(bs), s)
+	}
+}
+
+// BenchmarkConcat  1000000    64497 ns/op   502018 B/op   0 allocs/op
+// BenchmarkBuffer  100000000  15.5  ns/op   2 B/op        0 allocs/op
+// BenchmarkCopy    500000000  5.39  ns/op   0 B/op        0 allocs/op
