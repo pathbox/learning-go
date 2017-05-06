193687ea5b8a32c897440fd64f29253cb146428a
pathbox <1064569731@qq.com>
2017-05-06 12:51:19 +0800

    gowebapp app model

---
+++ src/gowebapp/vendor/app/model/model.go
@@ -0,0 +1,27 @@
+package model
+
+import (
+	"database/sql"
+	"errors"
+
+	"gopkg.in/mgo.v2"
+)
+
+var (
+	// ErrCode is a config or an internal error
+	ErrCode = errors.New("Case statement in code is not correct.")
+	// ErrNoResult is a not results error
+	ErrNoResult = errors.New("Result not found.")
+	// ErrUnavailable is a database not available error
+	ErrUnavailable = errors.New("Database is unavailable.")
+	// ErrUnauthorized is a permissions violation
+	ErrUnauthorized = errors.New("User does not have permission to perform this operation.")
+)
+
+func standardizeError(err error) error {
+	if err == sql.ErrNoRows || err == mgo.ErrNotFound {
+		return ErrNoResult
+	}
+
+	return err
+}
