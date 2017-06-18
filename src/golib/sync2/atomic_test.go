bae8df625daa3761ffd742305b81370bd233fc76
pathbox <1064569731@qq.com>
2017-06-18 10:55:25 +0800

    golib sync2

---
+++ src/golib/sync2/atomic_test.go
@@ -0,0 +1,47 @@
+package sync2
+
+import (
+	"testing"
+)
+
+func TestAtomicString(t *testing.T) {
+	var s AtomicString
+	if s.Get() != "" {
+		t.Errorf("want empty, got %s", s.Get())
+	}
+	s.Set("a")
+	if s.Get() != "a" {
+		t.Errorf("want a, got %s", s.Get())
+	}
+	if s.CompareAndSwap("b", "c") {
+		t.Errorf("want false, got true")
+	}
+	if s.Get() != "a" {
+		t.Errorf("want a, got %s", s.Get())
+	}
+	if !s.CompareAndSwap("a", "c") {
+		t.Errorf("want true, got false")
+	}
+	if s.Get() != "c" {
+		t.Errorf("want c, got %s", s.Get())
+	}
+}
+
+func TestAtomicBool(t *testing.T) {
+	var b AtomicBool
+	if b.Get() != false {
+		t.Fatal("must false")
+	}
+
+	b.Set(true)
+
+	if b.Get() != true {
+		t.Fatal("must true")
+	}
+
+	b.Set(false)
+
+	if b.Get() != false {
+		t.Fatal("must false")
+	}
+}
