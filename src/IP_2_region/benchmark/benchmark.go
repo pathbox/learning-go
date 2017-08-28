e7c4840ca85c1e56f11063f6d82a93e72e4f995f
luodaokai <luodaokai@udesk.cn>
2017-08-28 10:54:43 +0800

    ip2region 使用全局变量，可以达到最好的效果

--- src/IP_2_region/benchmark/benchmark.go
+++ src/IP_2_region/benchmark/benchmark.go
@@ -5,14 +5,14 @@ import (
 	"github.com/mohong122/ip2region/binding/golang"
 )
 
+var IP_SEARCH, _ = ip2region.New(".././data/ip2region.db")
+
 func MemorySearch() {
-	region, err := ip2region.New(".././ip2region.db")
-	defer region.Close()
-	if err != nil {
-		fmt.Println("============")
-		fmt.Println(err)
-		return
-	}
-	ip, err := region.BtreeSearch("123.95.223.18")
+
+	// IP_SEARCH.BinarySearch("123.95.223.18")
+	// IP_SEARCH.BtreeSearch("123.95.223.90")
+	IP_SEARCH.MemorySearch("123.95.223.90")
 	fmt.Println(ip, err)
 }
+
+// var IP_SEARCH, _ = ip2region.New(".././data/ip2region.db") //这里使用全局变量 可以达到最好的效果
