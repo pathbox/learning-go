2c41e022a294ea0c6be8adf0f1e8a38e16cc7820
pathbox <1064569731@qq.com>
2017-10-06 12:20:38 +0800

    build blockchain in go part2

---
+++ src/blockchain/blockchain_go/two/block.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"time"
+)
+
+// Block keeps block headers
+type Block struct {
+	Timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+	Nonce         int
+}
+
+// NewBlock creates and returns Block
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{
+		time.Now().Unix(),
+		[]byte(data),
+		prevBlockHash,
+		[]byte{},
+		0,
+	}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
+	return block
+}
+
+// NewGenesisBlock creates and returns genesis Block
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
