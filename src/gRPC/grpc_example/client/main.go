fa3e9796f4a2c683d4ac565cdb8398289eedb28a
luodaokai <luodaokai@udesk.cn>
2017-11-13 16:35:38 +0800

    gRPC example

---
+++ src/gRPC/grpc_example/client/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "log"
+
+    "context"
+    "google.golang.org/grpc"
+
+    "./../protobuf"
+)
+
+func main() {
+	// 连接到远端gRPC服务器
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer conn.Close()
+
+	// 建立新的Calculator客户端 所以等一下就能够使用Calculator的所有方法
+	c := protobuf.NewCalculatorClient(conn)
+
+	// 传送新请求到远端gRPC服务器Calculator中，并呼叫Plus函数
+	r, err := c.Plus(content.Background(), &protobuf.CalcRequest{NumberA: 32, NumberB: 88})
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	log.Println("得到的结果是： ", r.Result)
+}
+
+// 你可以直接在本地程式直接呼叫遠端程式，用法十分貼切。因為我們有 Protobuf 協助我們在客戶端和伺服端都共享同一份協定、結構
---
+++ src/gRPC/grpc_example/server/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+    "log"
+    "net"
+
+    "context"
+    "google.golang.org/grpc"
+    "google.golang.org/grpc/reflection"
+
+    "./../protobuf"
+)
+
+// server 结构体会作为 Calculator的gRPC服务器
+type server struct{}
+
+func (s *server) Plus(ctx context.Context, in *protobuf.CalcRequest) (*protobuf.CalcReply, error) {
+    result := in.NumberA + in.NumberB
+
+    // 包装成Protobuf 结构体并返回
+    return &protobuf.CalcReply{Result: result}, nil
+}
+
+func main() {
+    listen, err := net.Listen("tcp", ":50051")
+    if err != nil {
+        log.Fatalln(err)
+    }
+
+    // 建立新 gRPC服务器并注册Calculator服务
+    s := grpc.NewServer()
+    protobuf.RegisterCalculatorServer(s, &server{})
+
+    // 在gRPC服务器上注册反射服务
+    reflection.Register(s)
+
+    // 开始在指定的端口中服务
+    if err := s.Serve(listen); err != nil {
+        log.Fatalln(err)
+    }
+}
+
