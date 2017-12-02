1cb28ee3928b752dfde7a44847999d0ea5af1a97
pathbox <1064569731@qq.com>
2017-12-02 23:03:03 +0800

    websocket chat (Deadline method)

---
+++ src/websocket-example/gorilla-websocket/chat/hub.go
@@ -0,0 +1,46 @@
+package main
+
+// hub maintains the set of active clients and broadcasts messages to the
+// clients.
+
+type Hub struct {
+	clients map[*Client]bool
+
+	broadcast chan []byte
+
+	register chan *Client
+
+	unregister chan *Client
+}
+
+func newHub() *Hub {
+	return &Hub{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+	}
+}
+
+func (h *Hub) run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+		}
+	}
+}
