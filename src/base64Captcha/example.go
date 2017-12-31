d47b7523017281a0d7d260d42887c62fd1dbd011
pathbox <1064569731@qq.com>
2017-12-31 15:26:54 +0800

    base64 captcha example

---
+++ src/base64Captcha/example.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/mojocn/base64Captcha"
+	"log"
+	"net/http"
+)
+
+type ConfigJsonBody struct {
+	Id              string
+	CaptchaType     string
+	VerifyValue     string
+	ConfigAudio     base64Captcha.ConfigAudio
+	ConfigCharacter base64Captcha.ConfigCharacter
+	ConfigDigit     base64Captcha.ConfigDigit
+}
+
+func demoCode() {
+	//数字验证码配置
+	var configD = base64Captcha.ConfigDigit{
+		Height:     80,
+		Width:      240,
+		MaxSkew:    0.7,
+		DotCount:   80,
+		CaptchaLen: 5,
+	}
+
+	configA := base64Captcha.ConfigAudio{
+		CaptchaLen: 6,
+		Language:   "zh",
+	}
+
+	//字符,公式,验证码配置
+	var configC = base64Captcha.ConfigCharacter{
+		Height: 60,
+		Width:  240,
+		//const CaptchaModeNumber:数字,CaptchaModeAlphabet:字母,CaptchaModeArithmetic:算术,CaptchaModeNumberAlphabet:数字字母混合.
+		Mode:               base64Captcha.CaptchaModeNumber,
+		ComplexOfNoiseText: base64Captcha.CaptchaComplexLower,
+		ComplexOfNoiseDot:  base64Captcha.CaptchaComplexLower,
+		IsUseSimpleFont:    true,
+		IsShowHollowLine:   false,
+		IsShowNoiseDot:     false,
+		IsShowNoiseText:    false,
+		IsShowSlimeLine:    false,
+		IsShowSineLine:     false,
+		CaptchaLen:         6,
+	}
+	//create a audio captcha.
+	idKeyA, capA := base64Captcha.GenerateCaptcha("", configA)
+	//write to base64 string.
+	base64stringA := base64Captcha.CaptchaWriteToBase64Encoding(capA)
+	//create a characters captcha.
+	idKeyC, capC := base64Captcha.GenerateCaptcha("", configC)
+	//write to base64 string.
+	base64stringC := base64Captcha.CaptchaWriteToBase64Encoding(capC)
+	//create a digits captcha.
+	idKeyD, capD := base64Captcha.GenerateCaptcha("", configD)
+	//write to base64 string.
+	base64stringD := base64Captcha.CaptchaWriteToBase64Encoding(capD)
+
+	fmt.Println(idKeyA, base64stringA)
+	fmt.Println(idKeyC, base64stringC)
+	fmt.Println(idKeyD, base64stringD)
+
+}
+
+func generateCaptchaHandler(w http.ResponseWriter, r *http.Request) {
+	// parse request params
+	decoder := json.NewDecoder(r.Body)
+	var postParameters ConfigJsonBody
+	err := decoder.Decode(&postParameters) // json to struct
+	if err != nil {
+		log.Println(err)
+	}
+	defer r.Body.Close()
+
+	//create base64 encoding captcha
+	//创建base64图像验证码
+
+	var config interface{}
+	switch postParameters.CaptchaType {
+	case "audio":
+		config = postParameters.ConfigAudio
+	case "character":
+		config = postParameters.ConfigCharacter
+	default:
+		config = postParameters.ConfigDigit
+	}
+	captchaId, captcaInterfaceInstance := base64Captcha.GenerateCaptcha(postParameters.Id, config)
+	base64blob := base64Captcha.CaptchaWriteToBase64Encoding(captcaInterfaceInstance)
+	//set json response
+	//设置json响应
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	body := map[string]interface{}{"code": 1, "data": base64blob, "captchaId": captchaId, "msg": "success"}
+	json.NewEncoder(w).Encode(body) // body to json byte
+}
+
+// base64Captcha verify http handler
+func captchaVerifyHandle(w http.ResponseWriter, r *http.Request) {
+
+	//parse request parameters
+	//接收客户端发送来的请求参数
+	decoder := json.NewDecoder(r.Body)
+	var postParameters ConfigJsonBody
+	err := decoder.Decode(&postParameters)
+	if err != nil {
+		log.Println(err)
+	}
+	defer r.Body.Close()
+	//verify the captcha
+	//比较图像验证码
+	verifyResult := base64Captcha.VerifyCaptcha(postParameters.Id, postParameters.VerifyValue)
+
+	//set json response
+	//设置json响应
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	body := map[string]interface{}{"code": "error", "data": "验证失败", "msg": "captcha failed"}
+	if verifyResult {
+		body = map[string]interface{}{"code": "success", "data": "验证通过", "msg": "captcha verified"}
+	}
+	json.NewEncoder(w).Encode(body)
+}
+
+//start a net/http server
+//启动golang net/http 服务器
+func main() {
+
+	//serve Vuejs+ElementUI+Axios Web Application
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+
+	//api for create captcha
+	http.HandleFunc("/api/getCaptcha", generateCaptchaHandler)
+
+	//api for verify captcha
+	http.HandleFunc("/api/verifyCaptcha", captchaVerifyHandle)
+
+	fmt.Println("Server is at localhost:3333")
+	if err := http.ListenAndServe("localhost:3333", nil); err != nil {
+		log.Fatal(err)
+	}
+}
