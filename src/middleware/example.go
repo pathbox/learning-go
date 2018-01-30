56779c9d1390b4318c417f6243621324212d53d7
pathbox <guijihe@163.com>
2018-01-30 16:20:48 +0800

    understand the middleware example 理解middleware原理和执行顺序

---
+++ src/middleware/example.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func middlewareOne(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Executing middlewareOne")
+		next.ServeHTTP(w, r)
+		log.Println("Executing middlewareOne again")
+	})
+}
+func middlewareTwo(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Executing middlewareTwo")
+		if r.URL.Path != "/" {
+			return
+		}
+		next.ServeHTTP(w, r)
+		log.Println("Executing middlewareTwo again")
+	})
+}
+
+func final(w http.ResponseWriter, r *http.Request) {
+	log.Println("Executing finalHandler")
+	w.Write([]byte("OK"))
+}
+func main() {
+	finalHandler := http.HandlerFunc(final)
+
+	http.Handle("/", middlewareOne(middlewareTwo(finalHandler)))
+	http.ListenAndServe(":9000", nil)
+}
+
+/*
+next.ServeHTTP(w, r) 之前的代理逻辑是 队列,先进先出
+之后的逻辑是 栈 先进后出
+
+从外到内, 然后执行最后一个handler之后,再 从内到外
+*/
