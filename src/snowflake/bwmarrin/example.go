c3a96cb4dc7a4bc54a53f3bde1687e921552a618
pathbox <guijihe@163.com>
2018-02-07 14:45:31 +0800

    snowflake example

---
+++ src/snowflake/bwmarrin/example.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bwmarrin/snowflake"
+)
+
+func main() {
+	node, err := snowflake.NewNode(1)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	id := node.Generate()
+
+	// Print out the ID in a few different ways.
+	fmt.Printf("Int64  ID: %d\n", id)
+	fmt.Printf("String ID: %s\n", id)
+	fmt.Printf("Base2  ID: %s\n", id.Base2())
+	fmt.Printf("Base64 ID: %s\n", id.Base64())
+
+	// Print out the ID's timestamp
+	fmt.Printf("ID Time  : %d\n", id.Time())
+
+	// Print out the ID's node number
+	fmt.Printf("ID Node  : %d\n", id.Node())
+
+	// Print out the ID's sequence number
+	fmt.Printf("ID Step  : %d\n", id.Step())
+
+	// Generate and print, all in one.
+	fmt.Printf("ID       : %d\n", node.Generate().Int64())
+
+}
