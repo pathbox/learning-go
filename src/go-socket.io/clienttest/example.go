78d4d94ae159e58f4a0dd5b534340b3db8234c3e
pathbox <guijihe@163.com>
2018-03-31 15:40:13 +0800

    socket.io client go version

---
+++ src/go-socket.io/clienttest/example.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+
+	"github.com/zhouhui8915/go-socket.io-client"
+)
+
+func main() {
+
+	opts := &socketio_client.Options{
+		Transport: "websocket",
+		Query:     make(map[string]string),
+	}
+	opts.Query["user"] = "user"
+	opts.Query["pwd"] = "pass"
+	uri := "http://192.168.1.70:9090/socket.io/"
+
+	client, err := socketio_client.NewClient(uri, opts)
+	if err != nil {
+		log.Printf("NewClient error:%v\n", err)
+		return
+	}
+
+	client.On("error", func() {
+		log.Printf("on error\n")
+	})
+	client.On("connection", func() {
+		log.Printf("on connect\n")
+	})
+	client.On("message", func(msg string) {
+		log.Printf("on message:%v\n", msg)
+	})
+	client.On("disconnection", func() {
+		log.Printf("on disconnect\n")
+	})
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		data, _, _ := reader.ReadLine()
+		command := string(data)
+		client.Emit("message", command)
+		log.Printf("send message:%v\n", command)
+	}
+}
