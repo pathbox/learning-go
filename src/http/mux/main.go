37c94038b200e8c0ef62527211e9817c8bf4a80d
pathbox <1064569731@qq.com>
2018-04-11 14:16:44 +0800

    mux use mux.Vars(r) get id from url/{id:[0-9]+}

--- src/http/mux/main.go
+++ src/http/mux/main.go
@@ -3,17 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintln(w, "hello")
+	vars := mux.Vars(r)
+	fmt.Println(vars)
+	fmt.Println("++++++", vars["id"])
+
+	fmt.Fprintln(w, "hello")
 }
 
 func main() {
-  handlers := http.NewServeMux()
-  handlers.HandleFunc("/1", handler)
-  server := &http.Server{Addr: ":8080", Handler: handlers}
+	handlers := mux.NewRouter()
+	handlers.HandleFunc("/names/1", handler)
 
-  handlers.HandleFunc("/2", handler)
-  server.ListenAndServe()
+	handlers.HandleFunc("/2", handler)
+	handlers.HandleFunc("/agents/{id:[0-9]+}", handler)
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      handlers,
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+	}
+	server.ListenAndServe()
 }
