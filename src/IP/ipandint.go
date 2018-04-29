a5aa71b4279c1e66fcb0047fcaa21c2df6b31f0f
pathbox <1064569731@qq.com>
2018-04-29 17:27:45 +0800

    IP and int convert

---
+++ src/IP/ipandint.go
@@ -0,0 +1,35 @@
+import (
+	"net"
+	"strconv"
+	"strings"
+)
+
+// Convert uint to net.IP http://www.outofmemory.cn
+func inet_ntoa(ipnr int64) net.IP {
+	var bytes [4]byte
+	bytes[0] = byte(ipnr & 0xFF)
+	bytes[1] = byte((ipnr >> 8) & 0xFF)
+	bytes[2] = byte((ipnr >> 16) & 0xFF)
+	bytes[3] = byte((ipnr >> 24) & 0xFF)
+
+	return net.IPv4(bytes[3], bytes[2], bytes[1], bytes[0])
+}
+
+// Convert net.IP to int64 ,  http://www.outofmemory.cn
+func inet_aton(ipnr net.IP) int64 {
+	bits := strings.Split(ipnr.String(), ".")
+
+	b0, _ := strconv.Atoi(bits[0])
+	b1, _ := strconv.Atoi(bits[1])
+	b2, _ := strconv.Atoi(bits[2])
+	b3, _ := strconv.Atoi(bits[3])
+
+	var sum int64
+
+	sum += int64(b0) << 24
+	sum += int64(b1) << 16
+	sum += int64(b2) << 8
+	sum += int64(b3)
+
+	return sum
+}
