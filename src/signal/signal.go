ac350aca68672931ee2c69061f30e40f783e350f
pathbox <1064569731@qq.com>
2018-05-04 16:19:02 +0800

    signal  understand graceful

---
+++ src/signal/signal.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM, syscall.SIGUSR2)
+	for {
+		sig := <-ch
+
+		log.Println("signal: %v", sig)
+	}
+}
