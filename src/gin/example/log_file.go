7d2c08d1eff68d194c94c31169cda67cb2ccb70d
pathbox <1064569731@qq.com>
2018-05-04 11:09:30 +0800

    gin write log file

---
+++ src/gin/example/log_file.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"io"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Disable Console Color, you don't need console color when writing the logs to file.
+	gin.DisableConsoleColor()
+
+	// Logging to a file.
+	f, _ := os.Create("/logs/gin_server.log")
+	gin.DefaultWriter = io.MultiWriter(f)
+
+	// Use the following code if you need to write the logs to file and console at the same time.
+	// gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+
+	router.Run(":8080")
+}
