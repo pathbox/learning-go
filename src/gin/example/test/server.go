0f755820ca98912711047468ee7b025b71494b9e
pathbox <1064569731@qq.com>
2018-05-04 15:33:27 +0800

    gin test example

---
+++ src/gin/example/test/server.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+	return r
+}
+
+func main() {
+	r := setupRouter()
+	r.Run(":8080")
+}
