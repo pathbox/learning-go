2e41ca43ad5090560e4ec51f3c9319b0134632d5
pathbox <1064569731@qq.com>
2018-05-04 10:22:31 +0800

    gin Querystring parameters

---
+++ src/gin/example/querystring_parameters.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	// Query string parameters are parsed using the existing underlying request object.
+	// The request responds to a url matching:  /welcome?firstname=Jane&lastname=Doe
+	router.GET("/welcome", welcome)
+}
+
+func welcome(c *gin.Context) {
+
+	firstname := c.DefaultQuery("firstname", "Guest")
+	lastname := c.Query("lastname") // shortcut for c.Request.URL.Query().Get("lastname")
+
+	c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
+
+}
