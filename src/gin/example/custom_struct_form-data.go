2b5ff914de38906be1166dca489a0b35557a21a2
pathbox <1064569731@qq.com>
2018-05-04 15:25:27 +0800

    Bind form-data request with custom struct

---
+++ src/gin/example/custom_struct_form-data.go
@@ -0,0 +1,60 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+type StructA struct {
+	FieldA string `form:"field_a"`
+}
+
+type StructB struct {
+	NestedStruct StructA
+	FieldB       string `form:"field_b"`
+}
+
+type StructC struct {
+	NestedStructPointer *StructA
+	FieldC              string `form:"field_c"`
+}
+
+type StructD struct {
+	NestedAnonyStruct struct {
+		FieldX string `form:"field_x"`
+	}
+	FieldD string `form:"field_d"`
+}
+
+func GetDataB(c *gin.Context) {
+	var b StructB
+	c.Bind(&b)
+	c.JSON(200, gin.H{
+		"a": b.NestedStruct,
+		"b": b.FieldB,
+	})
+}
+
+func GetDataC(c *gin.Context) {
+	var b StructC
+	c.Bind(&b)
+	c.JSON(200, gin.H{
+		"a": b.NestedStructPointer,
+		"c": b.FieldC,
+	})
+}
+
+func GetDataD(c *gin.Context) {
+	var b StructD
+	c.Bind(&b)
+	c.JSON(200, gin.H{
+		"x": b.NestedAnonyStruct,
+		"d": b.FieldD,
+	})
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/getb", GetDataB)
+	r.GET("/getc", GetDataC)
+	r.GET("/getd", GetDataD)
+
+	r.Run()
+}
