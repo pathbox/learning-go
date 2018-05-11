8e3e5cc34b85fa21e3922aacef6f011682824ac6
pathbox <1064569731@qq.com>
2018-05-11 21:52:36 +0800

    networking

---
+++ src/networking/main.go
@@ -0,0 +1,279 @@
+package main
+
+import (
+	"bufio"
+	"encoding/gob"
+	"flag"
+	"io"
+	"log"
+	"net"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/pkg/errors"
+)
+
+type complexData struct {
+	N int
+	S string
+	M map[string]int
+	P []byte
+	C *complexData
+}
+
+const (
+	Port = ":61000"
+)
+
+func Open(addr string) (*bufio.ReadWriter, error) {
+	log.Println("Dial " + addr)
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		return nil, errors.Wrap(err, "Dialing "+addr+" failed")
+	}
+	return bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn)), nil
+}
+
+/*
+## Incoming connections
+Preparing for incoming data is a bit more involved. According to our ad-hoc
+protocol, we receive the name of a command terminated by `\n`, followed by data.
+The nature of the data depends on the respective command. To handle this, we
+create an `Endpoint` object with the following properties:
+* It allows to register one or more handler functions, where each can handle a
+  particular command.
+* It dispatches incoming commands to the associated handler based on the commands
+  name.
+*/
+
+// HandleFunc is a function that handles an incoming command.
+// It receives the open connection wrapped in a `ReadWriter` interface.
+
+type HandleFunc func(*bufio.ReadWriter)
+
+// Endpoint provides an endpoint to other processess
+// that they can send data to.
+type Endpoint struct {
+	listener net.Listener
+	handler  map[string]HandleFunc
+
+	m sync.RWMutex
+}
+
+func NewEndpoint() *Endpoint {
+	return &Endpoint{
+		handler: map[string]HandleFunc{},
+	}
+}
+
+func (e *Endpoint) AddHandleFunc(name string, f HandleFunc) {
+	e.m.Lock()
+	defer e.m.Unlock()
+	e.handler[name] = f
+}
+
+// Listen starts listening on the endpoint port on all interfaces.
+// At least one handler function must have been added
+// through AddHandleFunc() before.
+func (e *Endpoint) Listen() error {
+	var err error
+	e.listener, err = net.Listen("tcp", port)
+	if err != nil {
+		return errors.Wrapf(err, "Unable to listen on port %s\n", Port)
+	}
+	log.Println("Listen on", e.listener.Addr().String())
+	for {
+		log.Println("Accept a connection request.")
+		conn, err := e.listener.Accept()
+		if err != nil {
+			log.Println("Failed accepting a connection request:", err)
+			continue
+		}
+		log.Println("Handle incoming messages.")
+		go e.handleMessages(conn)
+	}
+}
+
+// handleMessages reads the connection up to the first newline.
+// Based on this string, it calls the appropriate HandleFunc.
+func (e *Endpoint) handleMessages(conn net.Conn) {
+	// Wrap the connection into a buffered reader for easier reading.
+	rw := bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
+	defer conn.Close()
+
+	// Read from the connection until EOF. Expect a command name as the
+	// next input. Call the handler that is registered for this command.
+	for {
+		log.Print("Receive command '")
+		cmd, err := rw.ReadString('\n')
+		switch {
+		case err == io.EOF:
+			log.Println("Reached EOF - close this connection.\n   ---")
+			return
+		case err != nil:
+			log.Println("\nError reading command. Got: '"+cmd+"'\n", err)
+			return
+		}
+		// Trim the request string - ReadString does not strip any newlines.
+		cmd = strings.Trim(cmd, "\n ")
+		log.Println(cmd + "'")
+
+		// Fetch the appropriate handler function from the 'handler' map and call it.
+		e.m.RLock()
+		handleCommand, ok := e.handler[cmd]
+		e.m.RUnlock()
+		if !ok {
+			log.Println("Command '" + cmd + "' is not registered.")
+			return
+		}
+		handleCommand(rw)
+	}
+}
+
+// handleStrings handles the "STRING" request.
+func handleStrings(rw *bufio.ReadWriter) {
+	// Receive a string.
+	log.Print("Receive STRING message:")
+	s, err := rw.ReadString('\n')
+	if err != nil {
+		log.Println("Cannot read from connection.\n", err)
+	}
+	s = strings.Trim(s, "\n ")
+	log.Println(s)
+	_, err = rw.WriteString("Thank you.\n")
+	if err != nil {
+		log.Println("Cannot write to connection.\n", err)
+	}
+	err = rw.Flush()
+	if err != nil {
+		log.Println("Flush failed.", err)
+	}
+}
+
+func handleGob(rw *bufio.ReadWriter) {
+	log.Print("Receive GOB data:")
+	var data complexData
+	dec := gob.NewDecoder(rw) // 从rw中读取数据
+	err := dec.Decode(&data)  // 将数据反序列化到data
+	if err != nil {
+		log.Println("Error decoding GOB data:", err)
+		return
+	}
+	// Print the complexData struct and the nested one, too, to prove
+	// that both travelled across the wire.
+	log.Printf("Outer complexData struct: \n%#v\n", data)
+	log.Printf("Inner complexData struct: \n%#v\n", data.C)
+}
+
+// client is called if the app is called with -connect=`ip addr`.
+func client(ip string) error {
+	// Some test data. Note how GOB even handles maps, slices, and
+	// recursive data structures without problems.
+	testStruct := complexData{
+		N: 23,
+		S: "string data",
+		M: map[string]int{"one": 1, "two": 2, "three": 3},
+		P: []byte("abc"),
+		C: &complexData{
+			N: 256,
+			S: "Recursive structs? Piece of cake!",
+			M: map[string]int{"01": 1, "10": 2, "11": 3},
+		},
+	}
+
+	// Open a connection to the server.
+	rw, err := Open(ip + Port)
+	if err != nil {
+		return errors.Wrap(err, "Client: Failed to open connection to "+ip+Port)
+	}
+
+	// Send a STRING request.
+	// Send the request name.
+	// Send the data.
+	log.Println("Send the string request.")
+	n, err := rw.WriteString("STRING\n")
+	if err != nil {
+		return errors.Wrap(err, "Could not send the STRING request ("+strconv.Itoa(n)+" bytes written)")
+	}
+	n, err = rw.WriteString("Additional data.\n")
+	if err != nil {
+		return errors.Wrap(err, "Could not send additional STRING data ("+strconv.Itoa(n)+" bytes written)")
+	}
+	log.Println("Flush the buffer.")
+	err = rw.Flush()
+	if err != nil {
+		return errors.Wrap(err, "Flush failed.")
+	}
+
+	// Read the reply.
+	log.Println("Read the reply.")
+	response, err := rw.ReadString('\n')
+	if err != nil {
+		return errors.Wrap(err, "Client: Failed to read the reply: '"+response+"'")
+	}
+
+	log.Println("STRING request: got a response:", response)
+
+	// Send a GOB request.
+	// Create an encoder that directly transmits to `rw`.
+	// Send the request name.
+	// Send the GOB.
+	log.Println("Send a struct as GOB:")
+	log.Printf("Outer complexData struct: \n%#v\n", testStruct)
+	log.Printf("Inner complexData struct: \n%#v\n", testStruct.C)
+	enc := gob.NewEncoder(rw)
+	n, err = rw.WriteString("GOB\n")
+	if err != nil {
+		return errors.Wrap(err, "Could not write GOB data ("+strconv.Itoa(n)+" bytes written)")
+	}
+	err = enc.Encode(testStruct)
+	if err != nil {
+		return errors.Wrapf(err, "Encode failed for struct: %#v", testStruct)
+	}
+	err = rw.Flush()
+	if err != nil {
+		return errors.Wrap(err, "Flush failed.")
+	}
+	return nil
+}
+
+// server listens for incoming requests and dispatches them to
+// registered handler functions.
+func server() error {
+	endpoint := NewEndpoint()
+
+	// Add the handle funcs.
+	endpoint.AddHandleFunc("STRING", handleStrings)
+	endpoint.AddHandleFunc("GOB", handleGob)
+
+	// Start listening.
+	return endpoint.Listen()
+}
+func main() {
+	connect := flag.String("connect", "", "IP address of process to join. If empty, go into listen mode.")
+	flag.Parse()
+
+	// If the connect flag is set, go into client mode.
+	if *connect != "" {
+		err := client(*connect)
+		if err != nil {
+			log.Println("Error:", errors.WithStack(err))
+		}
+		log.Println("Client done.")
+		return
+	}
+
+	// Else go into server mode.
+	err := server()
+	if err != nil {
+		log.Println("Error:", errors.WithStack(err))
+	}
+
+	log.Println("Server done.")
+}
+
+// The Lshortfile flag includes file name and line number in log messages.
+func init() {
+	log.SetFlags(log.Lshortfile)
+}
