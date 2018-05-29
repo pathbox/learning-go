e2757e688b2688823985dd12c1c60be61389c297
pathbox <1064569731@qq.com>
2018-05-29 18:24:34 +0800

    gin pprof

--- src/channel/channel-patterns/try-lock/example_timeout.go
+++ src/channel/channel-patterns/try-lock/example_timeout.go
@@ -1,42 +1,11 @@
 
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
+
+import "time"
+
 type Mutex struct {
 	ch chan struct{}
 }
+
 func NewMutex() *Mutex {
 	mu := &Mutex{make(chan struct{}, 1)}
 	mu.ch <- struct{}{}
@@ -58,7 +27,7 @@ func (m *Mutex) TryLock(timeout time.Duration) bool {
 	select {
 	case <-m.ch:
 		timer.Stop()
-		return true 
+		return true
 	case <-time.After(timeout):
 	}
 	return false
---
+++ src/gin/ginpprof/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/DeanThompson/ginpprof"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+
+	// automatically add routers for net/http/pprof
+	// e.g. /debug/pprof, /debug/pprof/heap, etc.
+	ginpprof.Wrap(router)
+
+	// ginpprof also plays well with *gin.RouterGroup
+	// group := router.Group("/debug/pprof")
+	// ginpprof.WrapGroup(group)
+
+	router.Run(":8080")
+}
