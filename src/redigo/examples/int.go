91a5dd69776ea7ac6a53cc887a8877fe347b8ea8
pathbox <1064569731@qq.com>
2018-07-08 17:49:30 +0800

    redigo int

---
+++ src/redigo/examples/int.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gomodule/redigo/redis"
+)
+
+func main() {
+	c, err := dial()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	c.Do("SET", "k1", 1)
+	n, _ := redis.Int(c.Do("GET", "k1"))
+	fmt.Printf("%#v\n", n)
+	n, _ = redis.Int(c.Do("INCR", "k1"))
+	fmt.Printf("%#v\n", n)
+	m, _ := redis.Int(c.Do("GET", "no")) // 如果去GET 不存在的key，会得到0的值，命令用小写也是可以的
+	fmt.Printf("%#v\n", m)
+}
+
+func dial() (redis.Conn, error) {
+	c, err := redis.Dial("tcp", ":6379")
+	if err != nil {
+		return nil, nil
+	}
+	return c, nil
+}
+
+//
