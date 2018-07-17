98a237805bd824e1679b94c843fa016ecaa28057
pathbox <cary.luo@ucloud.cn>
2018-07-17 15:41:22 +0800

    go-json-rest/statsd.go

---
+++ src/go-json-rest/statsd.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	statsd "github.com/ant0ine/go-json-rest-middleware-statsd"
+	"github.com/ant0ine/go-json-rest/rest"
+)
+
+func main() {
+	api := rest.NewApi()
+	api.Use(&statsd.StatsdMiddleware{})
+	api.Use(rest.DefaultDevStack...)
+	api.SetApp(rest.AppSimple(func(w rest.ResponseWriter, req *rest.Request) {
+
+		// take more than 1ms so statsd can report it
+		time.Sleep(100 * time.Millisecond)
+
+		w.WriteJson(map[string]string{"Body": "Hello World!"})
+	}))
+	log.Fatal(http.ListenAndServe(":8080", api.MakeHandler()))
+}
