9f9362773ebd499ae8b8dc4144d75a2499c1224d
pathbox <cary.luo@ucloud.cn>
2018-07-24 20:37:54 +0800

    base64 decoder zlib Reader

--- src/httputil/server.go
+++ src/httputil/server.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"compress/zlib"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"net/http/httputil"
 )
@@ -22,14 +24,44 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Printf("===The request content:%s\n", reqDump)
 
-	b, err := ioutil.ReadAll(r.Body)
+	// b, err := ioutil.ReadAll(r.Body)
+
+	// if err != nil {
+	// 	w.Write([]byte(string(err.Error())))
+	// 	return
+	// }
+
+	// fmt.Printf("===request body: %s\n", b)
+
+	// for name, headers := range r.Header {
+	// 	fmt.Printf("-name:%v---headers:%v-\n", name, headers)
+
+	// 	for _, h := range headers {
+	// 		fmt.Printf("%v:%v\n", name, h)
+	// 	}
+	// }
+
+	base64decoder := base64.NewDecoder(base64.StdEncoding, r.Body)
+	gz, err := zlib.NewReader(base64decoder) // 对r.Body 进行base64 编码后压缩
+
+	if err != nil {
+		fmt.Println("===err")
+		w.Write([]byte(string(err.Error())))
+		return
+	}
+
+	defer gz.Close()
+
+	decoder := json.NewDecoder(gz)
+	var t map[string]interface{}
+
+	err = decoder.Decode(&t)
 
 	if err != nil {
 		w.Write([]byte(string(err.Error())))
 		return
 	}
 
-	fmt.Printf("===request body: %s\n", b)
-
+	fmt.Printf("+++req result map: %v\n", t)
 	w.Write([]byte("Hello World!"))
 }
