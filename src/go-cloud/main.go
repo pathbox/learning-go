d557da63273675e779d0a90d6bd8e4e3d491d516
pathbox <cary.luo@ucloud.cn>
2018-07-25 19:51:11 +0800

    go cloud tutorial example

---
+++ src/go-cloud/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+	// Define our input.
+	cloud := flag.String("cloud", "", "Cloud storage to use")
+	bucketName := flag.String("bucket", "go-cloud-bucket", "Name of bucket")
+	flag.Parse()
+	if flag.NArg() != 1 {
+		log.Fatal("Failed to provide file to upload")
+	}
+	file := flag.Arg(0)
+
+	ctx := context.Background()
+	// Open a connection to the bucket.
+	b, err := setupBucket(context.Background(), *cloud, *bucketName)
+	if err != nil {
+		log.Fatalf("Failed to setup bucket: %s", err)
+	}
+
+	// Prepare the file for upload.
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatalf("Failed to read file: %s", err)
+	}
+
+	w, err := b.NewWriter(ctx, file, nil)
+	if err != nil {
+		log.Fatalf("Failed to obtain writer: %s", err)
+	}
+	_, err = w.Write(data)
+	if err != nil {
+		log.Fatalf("Failed to write to bucket: %s", err)
+	}
+	if err := w.Close(); err != nil {
+		log.Fatalf("Failed to close: %s", err)
+	}
+}
