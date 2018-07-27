2b699d440db1d9608c6f4d0b5ddead691102ec89
pathbox <cary.luo@ucloud.cn>
2018-07-27 14:53:18 +0800

    位运算-绝对值 补充

--- src/bit_operation/abs_value.go
+++ src/bit_operation/abs_value.go
@@ -3,13 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	a := -1
+	a := int64(-11)
 	fmt.Println(a)
 	r := abs(a)
 	fmt.Println(r)
 }
 
-func abs(a int) int {
-	r := a >> 32
-	return a ^ r - r
+func abs(a int64) int64 {
+	r := a >> 31
+
+	fmt.Println("r:", r)
+	// return a ^ r - r
+	if r == 0 { // 说明 a是正整数，直接返回
+		return a
+	} else {
+		return ^a + 1 // 说明a是负整数，取相反符号,即求正整数：反码+1
+	}
 }
