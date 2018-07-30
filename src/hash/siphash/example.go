4acbdc7c5cc466f6d5fa7464c56b3248bdd18063
pathbox <cary.luo@ucloud.cn>
2018-07-30 14:47:10 +0800

    hash siphash example

--- src/hash/siphash/example.go
+++ src/hash/siphash/example.go
@@ -3,11 +3,14 @@ package main
 import "fmt"
 import "github.com/dchest/siphash"
 
+const (
+	sipHashKey1 = 0xdda7806a4847ec61
+	sipHashKey2 = 0xb5940c2623a5aabd
+)
+
 func main() {
-	key := []byte("1")
-	h := siphash.New(key)
-	h.Write([]byte("Hello"))
-	// sum := h.Sum(nil)
-	sum := h.Sum64()
-	fmt.Println(sum)
+
+	x := []byte("100")
+	r := siphash.Hash(sipHashKey1, sipHashKey2, x)
+	fmt.Println(r)
 }
