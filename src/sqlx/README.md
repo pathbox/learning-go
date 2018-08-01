41925e74b039ba80d6facb51f1e177d112fe6f0e
pathbox <cary.luo@ucloud.cn>
2018-08-01 11:06:21 +0800

    sqlx example

---
+++ src/sqlx/README.md
@@ -0,0 +1,3 @@
+http://jmoiron.github.io/sqlx/
+
+https://github.com/jmoiron/sqlx
---
+++ src/sqlx/example.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+var schema = `
+CREATE TABLE person (
+    first_name text,
+    last_name text,
+    email text
+);
+
+CREATE TABLE place (
+    country text,
+    city text NULL,
+    telcode integer
+)`
+
+type Person struct {
+	FirstName string `db:"first_name"`
+	LastName  string `db:"last_name"`
+	Email     string
+}
+
+type Place struct {
+	Country string
+	City    sql.NullString
+	TelCode int
+}
+
+func main() {
+	// sqlx.Open is like sql.Open
+
+	db, err := sqlx.Connect("postgres", "user=foo dbname=mydatabase password=password sslmode=disable") // open and new a connection, just like  sqlx.Open  db.Ping()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	db.MustExec(schema)
+
+	tx := db.MustBegin()
+	tx.MustExec("INSERT INTO person (first_name, last_name, email) VALUES ($1,$2,$3)", "Jason", "Moiron", "Cary@google.net")
+	tx.MustExec("INSERT INTO person (first_name, last_name, email) VALUES ($1, $2, $3)", "John", "Doe", "johndoeDNE@gmail.net")
+	tx.MustExec("INSERT INTO place (country, city, telcode) VALUES ($1, $2, $3)", "United States", "New York", "1")
+	tx.MustExec("INSERT INTO place (country, telcode) VALUES ($1, $2)", "Hong Kong", "852")
+	tx.MustExec("INSERT INTO place (country, telcode) VALUES ($1, $2)", "Singapore", "65")
+	// Named queries can use structs, so if you have an existing struct (i.e. person := &Person{}) that you have populated, you can pass it in as &person
+	tx.NamedExec("INSERT INTO person (first_name, last_name, email) VALUES (:first_name, :last_name, :email)", &Person{"Jane", "Citizen", "jane.citzen@example.com"})
+
+	tx.Commit()
+
+	people := []Person{}
+	db.Select(&people, "SELECT * FROM person ORDER BY first_name ASC")
+	jason, john := people[0], people[1]
+
+	fmt.Printf("%#v\n%#v", jason, john)
+	// Person{FirstName:"Jason", LastName:"Moiron", Email:"jmoiron@jmoiron.net"}
+	// Person{FirstName:"John", LastName:"Doe", Email:"johndoeDNE@gmail.net"}
+
+	// You can also get a single result, a la QueryRow
+	jason = Person{}
+	err = db.Get(&jason, "SELECT * FROM person WHERE first_name=$1", "Jason")
+	fmt.Printf("%#v\n", jason)
+	// Person{FirstName:"Jason", LastName:"Moiron", Email:"jmoiron@jmoiron.net"}
+
+	// if you have null fields and use SELECT *, you must use sql.Null* in your struct
+	places := []Place{}
+	err = db.Select(&places, "SELECT * FROM place ORDER BY telcode ASC")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	usa, singsing, honkers := places[0], places[1], places[2]
+
+	fmt.Printf("%#v\n%#v\n%#v\n", usa, singsing, honkers)
+	// Place{Country:"United States", City:sql.NullString{String:"New York", Valid:true}, TelCode:1}
+	// Place{Country:"Singapore", City:sql.NullString{String:"", Valid:false}, TelCode:65}
+	// Place{Country:"Hong Kong", City:sql.NullString{String:"", Valid:false}, TelCode:852}
+
+	// Loop through rows using only one struct
+	place := Place{}
+	rows, err := db.Queryx("SELECT * FROM place")
+	for rows.Next() {
+		err := rows.StructScan(&place) // scan to struct
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Printf("%#v\n", place)
+	}
+
+	// Named queries, using `:name` as the bindvar.  Automatic bindvar support
+	// which takes into account the dbtype based on the driverName on sqlx.Open/Connect
+	_, err = db.NamedExec(`INSERT INTO person (first_name,last_name,email) VALUES (:first,:last,:email)`,
+		map[string]interface{}{
+			"first": "Bin",
+			"last":  "Smuth",
+			"email": "bensmith@allblacks.nz",
+		})
+
+	// Selects Mr. Smith from the database
+	rows, err = db.NamedQuery(`SELECT * FROM person WHERE first_name=:fn`, map[string]interface{}{"fn": "Bin"})
+
+	// Named queries can also use structs.  Their bind names follow the same rules
+	// as the name -> db mapping, so struct fields are lowercased and the `db` tag
+	// is taken into consideration.
+	rows, err = db.NamedQuery(`SELECT * FROM person WHERE first_name=:first_name`, jason) // jason is a struct and has the field first_name
+}
