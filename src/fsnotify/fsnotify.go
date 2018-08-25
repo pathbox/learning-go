c618ba923b2789c4cdb6919193e40ff5df8b8ea5
pathbox <cary.luo@ucloud.cn>
2018-08-25 14:20:39 +0800

    fsnotify/fsnotify.go

---
+++ src/fsnotify/fsnotify.go
@@ -0,0 +1,102 @@
+package fsnotify
+
+import "fmt"
+
+const ( //  权限的一种设定 0 1 2 4 8
+	FSN_CREATE = 1
+	FSN_MODIFY = 2
+	FSN_DELETE = 4
+	FSN_RENAME = 8
+
+	FSN_ALL = FSN_MODIFY | FSN_DELETE | FSN_RENAME | FSN_CREATE
+)
+
+//Purge events from interal chan to external chan if passes filter
+func (w *Watcher) purgeEvents() {
+	for ev := range w.internalEvent {
+		sendEvent := false
+		w.fsnmut.Lock()
+		fsnFlags := w.fsnFlags[ev.Name]
+		w.fsnmut.Unlock()
+
+		if (fsnFlags&FSN_CREATE == FSN_CREATE) && ev.IsCreate() {
+			sendEvent = true
+		}
+
+		if (fsnFlags&FSN_MODIFY == FSN_MODIFY) && ev.IsModify() {
+			sendEvent = true
+		}
+
+		if (fsnFlags&FSN_DELETE == FSN_DELETE) && ev.IsDelete() {
+			sendEvent = true
+		}
+
+		if (fsnFlags&FSN_RENAME == FSN_RENAME) && ev.IsRename() {
+			sendEvent = true
+		}
+
+		if sendEvent {
+			w.Event <- ev
+		}
+		// If there's no file, then no more events for user
+		// BSD must keep watch for internal use (watches DELETEs to keep track
+		// what files exist for create events)
+		if ev.IsDelete() {
+			w.fsnmut.Lock()
+			delete(w.fsnFlags, ev.Name)
+			w.fsnmut.Unlock()
+		}
+	}
+	close(w.Event)
+}
+
+func (w *Watcher) Watch(path string) error {
+	return w.WatchFlags(path, FSN_ALL)
+}
+
+// Watch a given file path for a particular set of notifications (FSN_MODIFY etc.)
+func (w *Watcher) WatchFlags(path string, flags uint32) error {
+	w.fsnmut.Lock()
+	w.fsnFlags[path] = flags
+	w.fsnmut.Unlock()
+	return w.watch(path)
+}
+
+// Remove a watch on a file
+func (w *Watcher) RemoveWatch(path string) error {
+	w.fsnmut.Lock()
+	delete(w.fsnFlags, path)
+	w.fsnmut.Unlock()
+	return w.removeWatch(path)
+}
+
+// String formats the event e in the form
+// "filename: DELETE|MODIFY|..."
+func (e *FilterEvent) String() string {
+	var events string = ""
+
+	if e.IsCreate() {
+		events += "|" + "CREATE"
+	}
+	if e.IsDelete() {
+		events += "|" + "DELETE"
+	}
+
+	if e.IsModify() {
+		events += "|" + "MODIFY"
+	}
+
+	if e.IsRename() {
+		events += "|" + "RENAME"
+	}
+
+	if e.IsAttrib() {
+		events += "|" + "ATTRIB"
+	}
+
+	if len(events) > 0 {
+		events = events[1:]
+	}
+
+	return fmt.Sprintf("%q: %s", e.Name, events)
+}
