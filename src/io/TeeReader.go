56ccce2be817d90ee433777d85f810ef9cca25f2
pathbox <cary.luo@ucloud.cn>
2018-08-31 16:00:35 +0800

    io.TeeReader: 数据会从origin reader 读取到 p 和 writer， TeeReader将reader和writer之间建立起了联系，便捷的将reader的数据传到writer

---
+++ src/io/TeeReader.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+
+	"strings"
+)
+
+func main() {
+	r := strings.NewReader("some io.Reader stream to be read\n") // origin reader
+
+	var buf bytes.Buffer // writer
+
+	tee := io.TeeReader(r, &buf)
+
+	p := make([]byte, 256)
+
+	tee.Read(p) // Read action
+	// data from origin reader to p and writer
+
+	fmt.Println("p Read content: ", string(p))
+
+	fmt.Println("Writer Recive: ", buf.String())
+
+}
+
+// 数据会从origin reader 读取到 p 和 writer， TeeReader将reader和writer之间建立起了联系，便捷的将reader的数据传到writer
--- src/io/multiReader.go
+++ src/io/multiReader.go
@@ -11,7 +11,7 @@ func main() {
 	r1 := strings.NewReader("first reader \n")
 	r2 := strings.NewReader("second reader \n")
 	r3 := strings.NewReader("third reader\n")
-	r := io.MultiReader(r2, r1, r3)
+	r := io.MultiReader(r2, r1, r3) // 按顺序处理Reader，前面的先处理
 
 	if _, err := io.Copy(os.Stdout, r); err != nil {
 		log.Fatal(err)
