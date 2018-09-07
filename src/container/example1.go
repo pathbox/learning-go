b9c15f6e49104aed68d31e0c9d6abde3938f7797
pathbox <cary.luo@ucloud.cn>
2018-09-07 16:25:25 +0800

    container/heap: 使用堆接口构建的整数堆

---
+++ src/container/example1.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+// 使用堆接口构建的整数堆
+type IntHeap []int
+
+// 实现三个接口方法
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+// 本示例将几个int插入到IntHeap中，检查最小值，
+// 并按优先顺序删除它们
+func main() {
+	h := &IntHeap{2, 1, 5}
+	heap.Init(h)
+	heap.Push(h, 3)
+	fmt.Printf("minimum: %d\n", (*h)[0])
+	for h.Len() > 0 {
+		fmt.Printf("%d ", heap.Pop(h))
+	}
+}
