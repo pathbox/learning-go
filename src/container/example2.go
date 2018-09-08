db35be5138274c172cce68c9fc9ce330c3a2ddbc
pathbox <cary.luo@ucloud.cn>
2018-09-08 20:28:49 +0800

    container/heap: example2 优先级队列

---
+++ src/container/example2.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+type Item struct {
+	value    string
+	priority int // 队列中item的优先级
+	index    int // 堆中item的参数
+}
+
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].priority > pq[j].priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	item.index = -1
+	*pq = old[0 : n-1]
+	return item // 返回n-1 最后一个元素，然后修改原pq为0-n-1个元素
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*Item)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+// 更新修改队列中某个item的优先级和值
+func (pq *PriorityQueue) update(item *Item, value string, priority int) {
+	item.value = value
+	item.priority = priority
+	heap.Fix(pq, item.index)
+}
+
+func main() {
+	items := map[string]int{
+		"banana": 3, "apple": 2, "pear": 4,
+	}
+
+	pq := make(PriorityQueue, len(items))
+	i := 0
+	for value, priority := range items {
+		pq[i] = &Item{
+			value:    value,
+			priority: priority,
+			index:    i,
+		}
+		i++
+	}
+
+	heap.Init(&pq)
+
+	item := &Item{
+		value:    "orange",
+		priority: 1,
+	}
+	heap.Push(&pq, item)
+	pq.update(item, item.value, 5) // orange priority 被更新为 5
+
+	for pq.Len() > 0 {
+		item := heap.Pop(&pq).(*Item)
+		fmt.Printf("%.2d:%s ", item.priority, item.value)
+	}
+}
