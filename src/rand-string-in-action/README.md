a24f4311d148db8c0d6917a5bd726428eb1443cc
pathbox <cary.luo@ucloud.cn>
2018-09-30 15:11:36 +0800

    rand string in action: rand.NewSource

--- src/rand-string-in-action/README.md
+++ src/rand-string-in-action/README.md
@@ -1,3 +1,11 @@
 https://colobu.com/2018/09/02/generate-random-string-in-Go/#more
 
 ##### 需要翻墙吧
+
+
+BenchmarkRunes                   1000000              1703 ns/op
+BenchmarkBytes                   1000000              1328 ns/op
+BenchmarkBytesRmndr              1000000              1012 ns/op
+BenchmarkBytesMask               1000000              1214 ns/op
+BenchmarkBytesMaskImpr           5000000               395 ns/op
+BenchmarkBytesMaskImprSrc        5000000               303 ns/op
---
+++ src/rand-string-in-action/rand_resource.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func main() {
+
+}
+
+var src = rand.NewSource(time.Now().UnixNano())
+
+func RandStringBytesMaskImprSrc(n int) string {
+	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+	return string(b)
+}
+
+func Int63() int64 { return globalRand.Int63() }
+
+var globalRand = New(&lockedSource{src: NewSource(1).(Source64)})
+
+type lockedSource struct {
+	lk  sync.Mutex
+	src Source64
+}
+
+func (r *lockedSource) Int63() (n int64) {
+	r.lk.Lock()
+	n = r.src.Int63()
+	r.lk.Unlock()
+	return
+}
