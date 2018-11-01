68c1aec84003fb297e7a086b0c4a0ec0296478e1
pathbox <cary.luo@ucloud.cn>
2018-11-01 16:36:47 +0800

    UFile put and post file example

--- src/UFile-example/upload_file.go
+++ src/UFile-example/upload_file.go
@@ -7,20 +7,36 @@ import (
 )
 
 const (
-	configFile1 = "config.json"
+	configFile1 = "./config.json"
 )
 
 func main() {
-	filePath := ""
+	filePath := "./test_file.txt"
 	config, err := ufsdk.LoadConfig(configFile1)
 	if err != nil {
 		panic(err.Error())
 	}
+
+	fileName1 := "test_file_1.txt" // 在ufile存储显示的文件名
 	req, _ := ufsdk.NewFileRequest(config, nil)
-	err = req.PutFile(filePath, "keyName", "")
+
+	err = req.PutFile(filePath, fileName1, "")
 	if err != nil {
 		fmt.Println("文件上传失败!!，错误信息为：", err.Error())
 		//把 HTTP 详细的 HTTP response dump 出来
-		fmt.Printf("===%s\n", req.DumpResponse(true))
+		fmt.Printf("%s\n", req.DumpResponse(true))
 	}
+
+	fmt.Println("Upload-Put OK:", string(req.DumpResponse(true)))
+
+	fileName2 := "test_file_2.txt"
+	err = req.PostFile(filePath, fileName2, "")
+	if err != nil {
+		fmt.Println("文件上传失败!!，错误信息为：", err.Error())
+		//把 HTTP 详细的 HTTP response dump 出来
+		fmt.Printf("%s\n", req.DumpResponse(true))
+	}
+
+	fmt.Println("Upload-Put OK:", string(req.DumpResponse(true)))
+
 }
