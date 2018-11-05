cc73a35b9d56bd6c9c129c2e0e43504522548b77
pathbox <cary.luo@ucloud.cn>
2018-11-05 10:26:40 +0800

    get function name: runtime.CallersFrames(pc[:n]) for trace every Caller

---
+++ src/get_function_name/example4.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	Bar()
+}
+
+func Bar() {
+	fmt.Printf("我是 %s, %s 又在调用我!\n", printMyName(), printCallerName())
+	trace()
+}
+func printMyName() string {
+	pc, _, _, _ := runtime.Caller(1)
+	return runtime.FuncForPC(pc).Name()
+}
+func printCallerName() string {
+	pc, _, _, _ := runtime.Caller(2)
+	return runtime.FuncForPC(pc).Name()
+}
+func trace() {
+	pc := make([]uintptr, 10) // at least 1 entry needed
+	n := runtime.Callers(0, pc)
+	frames := runtime.CallersFrames(pc[:n])
+	for {
+		frame, more := frames.Next()
+		fmt.Printf("%s:%d %s\n", frame.File, frame.Line, frame.Function)
+		if !more {
+			break
+		}
+	}
+}
