445f7d7106a5dec22d520fdc55b70671400d9b75
pathbox <cary.luo@ucloud.cn>
2018-11-18 14:41:28 +0800

    gg:Go Graphics - 2D rendering in Go-example: circle

---
+++ src/gg/examples/circle.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/fogleman/gg"
+)
+
+func main() {
+	dc := gg.NewContext(1000, 1000)
+	dc.DrawCircle(500, 500, 500)
+	dc.SetRGB(0, 0, 0)
+	dc.Fill()
+	dc.SavePNG("out.png")
+}
