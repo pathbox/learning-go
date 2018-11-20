569cea9217224f37b54530ca53329736c4d5a4cf
pathbox <cary.luo@ucloud.cn>
2018-11-20 10:10:04 +0800

    q:It is a nice cool awesome debug way in Go

---
+++ src/q/README.md
@@ -0,0 +1,3 @@
+https://github.com/y0ssar1an/q
+
+It is a nice cool awesome debug way in Go
---
+++ src/q/example.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/y0ssar1an/q"
+)
+
+func main() {
+	s := "O ha you!"
+	q.Q(s, s, s)
+}
+
+// tail -f $TMPDIR/q
