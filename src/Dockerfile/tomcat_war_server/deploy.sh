7f2d349fdb550685eeafeaf3b3fc2b6c46549d78
pathbox <cary.luo@ucloud.cn>
2018-12-28 15:28:03 +0800

    增加挂载目录，将日志目录挂载到宿主机

--- src/Dockerfile/tomcat_war_server/deploy.sh
+++ src/Dockerfile/tomcat_war_server/deploy.sh
@@ -2,7 +2,7 @@
 
 docker build -t my_tomcat_war/tomcat:v1 .
 
-docker run -d -p 8080:8080 my_tomcat_war/tomcat:v1 # 第二个端口号为docker中Tomcat war包服务的端口号，需要和server.xml中的配置的端口号一致，默认是8080
+docker run -v /tmp/my_war_logs:/usr/local/tomcat/logs -d -p 8080:8080 my_tomcat_war/tomcat:v1 # 第二个端口号为docker中Tomcat war包服务的端口号，需要和server.xml中的配置的端口号一致，默认是8080 挂载的日志目录，docker运行用户要有读写和进入的权限
 
 sleep 5 # 睡眠5秒等待Tomcat war完全启动
 
