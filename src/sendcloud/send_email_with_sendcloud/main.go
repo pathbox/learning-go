bafb1140c400ba7e9279c9b464f45a5fab9213eb
pathbox <cary.luo@ucloud.cn>
2019-01-13 11:23:46 +0800

    fix action

--- src/sendcloud/send_email_with_sendcloud/main.go
+++ src/sendcloud/send_email_with_sendcloud/main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net"
 	"net/http"
-	"net/smtp"
 	"strings"
 	"time"
 
@@ -14,46 +12,15 @@ import (
 )
 
 const (
-	imagePath1    = "./image.jpg"
-	imagePath2    = ""
-	subjectPath   = "./subject.txt"
-	bodyPath      = "./body.txt"
 	excelFileName = "./email_list.xlsx"
 )
 
-type SendMail struct {
-	user     string
-	password string
-	host     string
-	port     string
-	auth     smtp.Auth
-}
-
-type Attachment struct {
-	name        []string
-	contentType string
-	withFile    bool
-}
-
-type Message struct {
-	from        string
-	to          string
-	cc          []string
-	bcc         []string
-	subject     string
-	body        string
-	contentType string
-	attachment  Attachment
-}
-
 func main() {
-	from := "postmaster@fintechgood.com"
-	// subs := map[string]interface{}{}
-
-	apiUser := "fintech_send"
-	apiKey := "apikey"
+	from := "xxx@domain"
+	apiUser := "xxx"
+	apiKey := "xxx"
 	sendcloud.UpdateApiInfo(apiUser, apiKey)
-	tplName := "fintech_sendcloud_template"
+	tplName := "xxxtemplate"
 
 	xlFile, err := xlsx.OpenFile(excelFileName)
 	if err != nil {
@@ -83,50 +50,50 @@ func main() {
 	}
 }
 
-func (mail SendMail) SendWithSendCloud(from, to, subject string, buf []byte) error {
-	var r http.Request
-	r.ParseForm()
-	client := NewHTTPClient()
-	url := "http://api.sendcloud.net/apiv2/mail/send"
+// func (mail SendMail) SendWithSendCloud(from, to, subject string, buf []byte) error {
+// 	var r http.Request
+// 	r.ParseForm()
+// 	client := NewHTTPClient()
+// 	url := "http://api.sendcloud.net/apiv2/mail/send"
 
-	fmt.Println("len:", len(buf))
+// 	fmt.Println("len:", len(buf))
 
-	paramMap := make(map[string]string)
-	paramMap["apiUser"] = " "
-	paramMap["apiKey"] = " "
-	paramMap["from"] = from
-	paramMap["fromName"] = from
-	paramMap["to"] = to
-	paramMap["subject"] = subject
-	paramMap["html"] = string(buf)
-	paramMap["respEmailId"] = "true"
+// 	paramMap := make(map[string]string)
+// 	paramMap["apiUser"] = " "
+// 	paramMap["apiKey"] = " "
+// 	paramMap["from"] = from
+// 	paramMap["fromName"] = from
+// 	paramMap["to"] = to
+// 	paramMap["subject"] = subject
+// 	paramMap["html"] = string(buf)
+// 	paramMap["respEmailId"] = "true"
 
-	for k, v := range paramMap {
-		r.Form.Add(k, v)
-	}
+// 	for k, v := range paramMap {
+// 		r.Form.Add(k, v)
+// 	}
 
-	payload := strings.TrimSpace(r.Form.Encode())
-	request, err := http.NewRequest("POST", url, strings.NewReader(payload))
-	if err != nil {
-		return err
-	}
+// 	payload := strings.TrimSpace(r.Form.Encode())
+// 	request, err := http.NewRequest("POST", url, strings.NewReader(payload))
+// 	if err != nil {
+// 		return err
+// 	}
 
-	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	resp, err := client.Do(request)
-	if err != nil {
-		return err
-	}
+// 	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+// 	resp, err := client.Do(request)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
+// 	body, err := ioutil.ReadAll(resp.Body)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer resp.Body.Close()
 
-	fmt.Printf("Response:%s\n", body)
+// 	fmt.Printf("Response:%s\n", body)
 
-	return nil
-}
+// 	return nil
+// }
 
 func NewHTTPClient() *http.Client {
 	transport := &http.Transport{
