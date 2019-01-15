e1498a8470af2ec8707590e70b9436f60499912b
pathbox <cary.luo@ucloud.cn>
2019-01-15 10:35:27 +0800

    construct pipelines example

---
+++ src/concurrency/construct_pipelines.go
@@ -0,0 +1,68 @@
+package main
+
+import "fmt"
+
+func main() {
+	generator := func(done <-chan interface{}, integers ...int) <-chan int {
+		intStreaam := make(chan int)
+		go func() {
+			defer close(intStreaam)
+			for _, i := range integers {
+				select {
+				case <-done:
+					return
+				case intStreaam <- i:
+				}
+			}
+		}()
+		return intStreaam
+	}
+
+	multiply := func(
+		done <-chan interface{},
+		intStream <-chan int,
+		multiplier int,
+	) <-chan int {
+		multipliedStream := make(chan int)
+		go func() {
+			defer close(multipliedStream)
+			for i := range intStream {
+				select {
+				case <-done:
+					return
+				case multipliedStream <- i * multiplier:
+				}
+			}
+		}()
+		return multipliedStream
+	}
+
+	add := func(
+		done <-chan interface{},
+		intStream <-chan int,
+		additive int,
+	) <-chan int {
+		addedStream := make(chan int)
+		go func() {
+			defer close(addedStream)
+			for i := range intStream {
+				select {
+				case <-done:
+					return
+				case addedStream <- i + additive:
+				}
+			}
+		}()
+		return addedStream
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	intStreaam := generator(done, 1, 2, 3, 4)
+	pipeline := multiply(done, add(done, multiply(done, intStream, 2), 1), 2)
+
+	for v := range pipeline {
+		fmt.Println(v)
+	}
+}
