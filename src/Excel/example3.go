4249264df31dff11a1c842e9ae8a578480fc0dfe
pathbox <cary.luo@ucloud.cn>
2019-05-14 10:34:52 +0800

    read excel to big map

---
+++ src/Excel/example3.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/360EntSecGroup-Skylar/excelize"
+)
+
+func main() {
+	xlsxFile := "/Users/pathbox/model.xlsx"
+	m, _ := ReadCustomerFromExcel(xlsxFile)
+	fmt.Printf("%+v", m)
+}
+
+func ReadCustomerFromExcel(xlsxFile string) (map[string][]map[string]interface{}, error) {
+	m := map[string][]map[string]interface{}{}
+	xFile, err := excelize.OpenFile(xlsxFile)
+	if err != nil {
+		fmt.Println(err)
+		return m, err
+	}
+
+	for _, name := range xFile.GetSheetMap() {
+		rows, _ := xFile.GetRows(name)
+		cuSlice := make([]map[string]interface{}, 0)
+		for index, row := range rows {
+			if index == 0 {
+				continue
+			}
+			asix := fmt.Sprintf("A%d", index+1)
+			_, url, _ := xFile.GetCellHyperLink(name, asix)
+			row = append(row, url)
+			// fmt.Println(row)
+
+			cu := map[string]interface{}{}
+			cu["Name"] = row[0]
+			cu["University"] = row[1]
+			cu["CraduateYear"] = row[2]
+			cu["Company"] = row[3]
+			cu["Title"] = row[4]
+			cu["ExperienceYear"] = row[5]
+			cu["Email"] = row[6]
+			cu["Phone"] = row[7]
+			cu["Wechat"] = row[8]
+			cu["Address"] = row[9]
+			cu["HomeURL"] = row[10]
+			cuSlice = append(cuSlice, cu)
+		}
+		m[name] = cuSlice
+	}
+	return m, nil
+}
