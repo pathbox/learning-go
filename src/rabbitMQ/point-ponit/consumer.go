6e68750258fc6a9f9ff2f1c1639de0af321eb543
pathbox <guijihe@163.com>
2019-09-14 21:25:49 +0800

    rabbitMQ point-point consumer

--- src/rabbitMQ/point-ponit/consumer.go
+++ src/rabbitMQ/point-ponit/consumer.go
@@ -29,6 +29,43 @@ func main() {
 		"task_name",
 		true, // durable 和生产者设置保持一致
 		false, // delete when unused
-		false, // exclusive
+		false, // 如果是true，连接一断开 队列就会删除
+		false,
+		nil,
 	)
+	failOnError(err,"")
+
+	err = ch.Qos(
+		1, // prefetch count
+		0, 
+		false, 
+	)
+	failOnError(err,"")
+
+	// 消费者根据路由的队列名
+	msgs, err := ch.Consume(
+		queue.Name, 
+		"cpmsumer name", 
+		false, // auto-ask
+		false, //exclusive
+		false, // no-local
+		false, // no-wait
+		nil, //args 
+	)
+	failOnError(err, "")
+	forever := make(chan bool)
+
+	go func() { // 异步从msgs 消费者中消费msg
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+			dot_count := bytes.Count(d.Body, []byte("."))
+			t := time.Duration(dot_count)
+			time.Sleep(t * time.Second)
+			log.Printf("Done")
+			// 确认消息被收到！！如果为真的，那么同在一个channel,在该消息之前未确认的消息都会确认，适合批量处理
+      d.Ack(false)
+		}
+	}()
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever // 不让consumer main goroutine结束
 }
