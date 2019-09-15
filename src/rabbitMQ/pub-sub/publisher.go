7f5eba0936490162dfc5f65c36383059608739ea
pathbox <guijihe@163.com>
2019-09-15 11:44:49 +0800

    rabbitMQ pub-sub-direct example

---
+++ src/rabbitMQ/pub-sub/publisher.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+        "fmt"
+        "log"
+        "os"
+        "strings"
+
+        "github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+        if err != nil {
+                log.Fatalf("%s: %s", msg, err)
+                panic(fmt.Sprintf("%s: %s", msg, err))
+        }
+}
+
+func main() {
+        conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+        failOnError(err, "Failed to connect to RabbitMQ")
+        defer conn.Close()
+
+        ch, err := conn.Channel()
+        failOnError(err, "Failed to open a channel")
+        defer ch.Close()
+
+        // 交换机申明，且类型为点对点默认
+        err = ch.ExchangeDeclare(
+                "logs_direct", // name
+                "direct",      // type
+                true,          // durable
+                false,         // auto-deleted
+                false,         // internal
+                false,         // no-wait
+                nil,           // arguments
+        )
+        failOnError(err, "Failed to declare an exchange")
+
+        body := bodyFrom(os.Args)
+        
+        // 发布
+        err = ch.Publish(
+                "logs_direct",         // exchange   发到这个交换机
+                severityFrom(os.Args), // routing key  且路由key是由命令行指定，如下方，指定了error    
+                false, // mandatory
+                false, // immediate
+                amqp.Publishing{
+                        ContentType: "text/plain",
+                        Body:        []byte(body),
+                })
+        failOnError(err, "Failed to publish a message")
+
+        log.Printf(" [x] Sent %s", body)
+}
+
+func bodyFrom(args []string) string {
+        var s string
+        if (len(args) < 3) || os.Args[2] == "" {
+                s = "hello"
+        } else {
+                s = strings.Join(args[2:], " ")
+        }
+        return s
+}
+
+func severityFrom(args []string) string {
+        var s string
+        if (len(args) < 2) || os.Args[1] == "" {
+                s = "info"
+        } else {
+                s = os.Args[1]
+        }
+        return s
+}
