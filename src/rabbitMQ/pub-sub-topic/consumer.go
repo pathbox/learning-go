39d9026d4a2ba5644de5372473b66daf08ae2ae7
pathbox <guijihe@163.com>
2019-09-15 11:46:51 +0800

    rabbitMQ pub-sub-topic example

---
+++ src/rabbitMQ/pub-sub-topic/consumer.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+        "fmt"
+        "log"
+        "os"
+
+        "github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+        if err != nil {
+                log.Fatalf("%s: %s", msg, err)
+                panic(fmt.Sprintf("%s: %s", msg, err))
+        }
+}
+
+func main() {
+        conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+        failOnError(err, "Failed to connect to RabbitMQ")
+        defer conn.Close()
+
+        ch, err := conn.Channel()
+        failOnError(err, "Failed to open a channel")
+        defer ch.Close()
+
+
+        // 同理
+        err = ch.ExchangeDeclare(
+                "logs_topic", // name
+                "topic",      // type
+                true,         // durable
+                false,        // auto-deleted
+                false,        // internal
+                false,        // no-wait
+                nil,          // arguments
+        )
+        failOnError(err, "Failed to declare an exchange")
+
+        // 临时队列
+        q, err := ch.QueueDeclare(
+                "",    // name
+                false, // durable
+                false, // delete when usused
+                true,  // exclusive
+                false, // no-wait
+                nil,   // arguments
+        )
+        failOnError(err, "Failed to declare a queue")
+
+        if len(os.Args) < 2 {
+                log.Printf("Usage: %s [binding_key]...", os.Args[0])
+                os.Exit(0)
+        }
+        for _, s := range os.Args[1:] {
+                log.Printf("Binding queue %s to exchange %s with routing key %s",
+                        q.Name, "logs_topic", s)
+                // 绑定也是类似的
+                err = ch.QueueBind(
+                        q.Name,       // queue name
+                        s,            // routing key
+                        "logs_topic", // exchange
+                        false,
+                        nil)
+                failOnError(err, "Failed to bind a queue")
+        }
+
+        msgs, err := ch.Consume(
+                q.Name, // queue
+                "",     // consumer
+                true,   // auto ack
+                false,  // exclusive
+                false,  // no local
+                false,  // no wait
+                nil,    // args
+        )
+        failOnError(err, "Failed to register a consumer")
+
+        forever := make(chan bool)
+
+        go func() {
+                for d := range msgs {
+                        log.Printf(" [x] %s", d.Body)
+                }
+        }()
+
+        log.Printf(" [*] Waiting for logs. To exit press CTRL+C")
+        <forever
+}
