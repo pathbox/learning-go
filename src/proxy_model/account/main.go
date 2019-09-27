5d2f6a145e667b384a8f43d1e1f643e116919ff1
pathbox <cary.luo@ucloud.cn>
2019-09-27 17:52:27 +0800

    AccountImpl实现了Account接口

--- src/proxy_model/account/main.go
+++ src/proxy_model/account/main.go
@@ -14,8 +14,8 @@ func main() {
 
 func init() {
     account.New = func(id, name string, value int) account.Account {
-        a := &account.AccountImpl{id, name, value}
-        p := &proxy.Proxy{a}
+        a := &account.AccountImpl{id, name, value} // AccountImpl实现了Account接口
+        p := &proxy.Proxy{a} // 所以可以带到Proxy中
         return p
     }
 }
