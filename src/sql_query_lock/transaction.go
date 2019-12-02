a83589cbe011a3f674eba9b8225a44e44ef0eb6b
pathbox <cary.luo@ucloud.cn>
2019-12-02 16:05:02 +0800

    构造IN查询的方式

--- src/sql_query_lock/transaction.go
+++ src/sql_query_lock/transaction.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"strings"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -25,19 +26,52 @@ func main() {
 	db.SetMaxOpenConns(20)
 
 	transaction(db)
-	c := make(chan int)
-	<-c
 }
 
 func transaction(db *sql.DB) {
-
+	sqlQ := `SELECT id FROM article limit 10`
 	sqlStr1 := `UPDATE article SET title = 'It is a start for transaction' WHERE id = 11;`
 	sqlStr2 := `UPDATE article SET title = 'It is a end for transaction' WHERE id = 11;`
+	rows, _ := db.Query(sqlQ)
+	defer rows.Close()
+	idAry := make([]string, 0)
+	for rows.Next() {
+		var id string
+		err := rows.Scan(&id)
+		if err != nil {
+			fmt.Println(err)
+		}
+		idAry = append(idAry, id)
+		fmt.Println("===", id)
+	}
+
+	fmt.Println("===", idAry)
+
+	// sqlQ2 := `SELECT title FROM article WHERE id IN(?)` // This is not right
+	sqlQ2 := fmt.Sprintf("SELECT title FROM article WHERE id IN(%s)", strings.Join(idAry, ",")) // This is right
+	fmt.Println("sqlQ2:", sqlQ2)
+	rows2, err := db.Query(sqlQ2)
+	if err != nil {
+		fmt.Println(rows2.Err())
+	}
+	defer rows2.Close()
+
+	for rows2.Next() {
+		var title string
+		err = rows2.Scan(&title)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("XXX", title)
+	}
+
+	return
 
 	tx, _ := db.Begin()
 	defer tx.Rollback()
 	// _, err := db.Exec(sqlStr1)
-	_, err := tx.Exec(sqlStr1) // 这样就对啦！
+	_, err = tx.Exec(sqlStr1) // 这样就对啦！
 	if err != nil {
 		panic(err)
 	}
