da43d1ad3561e01dc7e40bb3326e5c14ed32d93c
pathbox <cary.luo@ucloud.cn>
2019-12-13 16:09:53 +0800

    通过域名得到域名ip

---
+++ src/host_to_ip/host_to_ip.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	addr, err := net.LookupHost("ucloud.cn")
+	if err, ok := err.(*net.DNSError); ok {
+		if err.Timeout() {
+			fmt.Println("operation timed out")
+		} else if err.Temporary() {
+			fmt.Println("temporary error")
+		} else {
+			fmt.Println("generic error: ", err)
+		}
+		return
+	}
+	fmt.Println(addr)
+}
