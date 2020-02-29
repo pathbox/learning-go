05734e95375341030434998bfde91ab9ee15dc97
pathbox <cary.luo@ucloud.cn>
2020-02-29 15:30:12 +0800

    Write ahead log for Go:example

---
+++ src/wal/example.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/tidwall/wal"
+)
+func main() {
+	log, err := wal.Open("mylog",nil)
+	// write some entries
+	err = log.Write(1, []byte("first entry"))
+	err = log.Write(2, []byte("second entry"))
+	err = log.Write(3, []byte("third entry"))
+
+	// read an entry
+	data, err := log.Read(1)
+	println(string(data))  // output: first entry
+	if err != nil {
+		panic(err)
+	}
+
+	// close the log
+	log.Close()		
+
+}
