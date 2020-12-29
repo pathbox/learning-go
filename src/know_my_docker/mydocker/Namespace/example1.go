02f480f9bfe8d3959dbf5657eeca2150f03de74c
pathbox <cary.luo@ucloud.cn>
2020-12-29 17:12:46 +0800

    know my docker:Namespace example1

---
+++ src/know_my_docker/mydocker/Namespace/example1.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	cmd := exec.Command("sh")
+	cmd.SysProcAttr = &syscall.SysProcAttr{}
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
