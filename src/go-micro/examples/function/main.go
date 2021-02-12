96001f826fef9e10869d32b2efa4d1f826c22a9c
pathbox <cary.luo@ucloud.cn>
2021-02-12 12:02:43 +0800

    feat:go-micro examples:function

---
+++ src/go-micro/examples/function/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+
+	proto "github.com/asim/go-micro/examples/v3/function/proto"
+	"github.com/asim/go-micro/v3"
+)
+
+type Greeter struct{}
+
+func (g *Greeter) Hello(ctx context.Context, req *proto.HelloRequest, rsp *proto.HelloResponse) error {
+	rsp.Greeting = "Hello " + req.Name
+	return nil
+}
+
+func main() {
+	fnc := micro.NewFunction(
+		micro.Name("greeter"),
+	)
+
+	fnc.Init()
+
+	fnc.Handle(new(Greeter))
+
+	fnc.Run()
+}
+
+// micro call greeter Greeter.Hello '{"name": "john"}'
