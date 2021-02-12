9bc9507f527e6383c1c332769627cef347fb8f64
pathbox <cary.luo@ucloud.cn>
2021-02-12 11:58:46 +0800

    feat:go-micro examples:filter

---
+++ src/go-micro/examples/filter/main.go
@@ -0,0 +1,31 @@
+// Filter demonstrates how to filter requests in the client
+
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/asim/go-micro/examples/v3/filter/version"
+	proto "github.com/asim/go-micro/examples/v3/service/proto"
+	"github.com/asim/go-micro/v3"
+)
+
+func main() {
+	greeter := proto.NewGreeterService("greeter", service.Client())
+
+	rsp, err := greeter.Hello(
+		// provide a context
+		context.TODO(),
+		// provide the request
+		&proto.Request{Name: "John"},
+		// set the filter
+		version.Filter("latest"),
+	)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(rsp.Greeting)
+}
