5ade7db661e0d4a77ea000d29def04f8dc7faadb
pathbox <cary.luo@ucloud.cn>
2021-03-18 10:57:13 +0800

    kubernetes-dashboard

--- src/slice/grow_way.go
+++ src/slice/grow_way.go
@@ -20,3 +20,4 @@ func main() {
 }
 
 // 如果只看前半部分，现在网上各种文章里说的 newcap 的规律是对的。现实是，后半部分还对 newcap 作了一个内存对齐，这个和内存分配策略相关。进行内存对齐之后，新 slice 的容量是要 大于等于 老 slice 容量的 2倍或者1.25倍
+// https://golang.design/go-questions/slice/grow/
