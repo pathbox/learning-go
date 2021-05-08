0dad0fa6701bb5a2c1257f24513f5de1c1ffcbec
pathbox <cary.luo@ucloud.cn>
2021-05-08 15:16:27 +0800

    simple REPL

---
+++ src/9t/tesgo.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{2, 13, 15, 6, 7}
+	n := len(arr)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	fmt.Println(arr)
+}
